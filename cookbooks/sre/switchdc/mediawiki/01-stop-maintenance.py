6f3dcf48513d7ceb7d68b61535c58cb19e7ab9b7
Riccardo Coccioli <rcoccioli@wikimedia.org>
2018-08-30 22:55:17 +0200

    sre.switchdc.mediawiki: add Phase 1 cookbook
    * Add the Phase 1 cookbook of the MediaWiki switchdc, see:
      https://wikitech.wikimedia.org/wiki/Switch_Datacenter
    * 01-stop-maintenance: Stop MediaWiki mainteance jobs.
    
    Bug: T199079
    Change-Id: I6ec3cd71e4b36b37d9a0bf7896af648622e9633f

---
+++ cookbooks/sre/switchdc/mediawiki/01-stop-maintenance.py
@@ -0,0 +1,16 @@
+"""Stop MediaWiki mainteance jobs"""
+import logging
+
+from cookbooks.sre.switchdc.mediawiki import parse_args
+
+
+__title__ = __doc__
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+def main(args, spicerack):
+    """Required by Spicerack API."""
+    args = parse_args(__name__, __title__, args)
+
+    logger.info('Stopping MediaWiki mainteance jobs in %s', args.dc_from)
+    spicerack.mediawiki().stop_cronjobs(args.dc_from)
