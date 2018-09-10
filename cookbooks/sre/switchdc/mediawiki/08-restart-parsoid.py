5c791046a9932340c9ec0ef8a14913df80492da4
Riccardo Coccioli <rcoccioli@wikimedia.org>
2018-09-10 20:44:47 +0200

    sre.switchdc.mediawiki: parsoid skip broken host
    Bug: T199079
    Change-Id: Ie5c60432cf68c351242aaacb4106f7a52914468f

--- cookbooks/sre/switchdc/mediawiki/08-restart-parsoid.py
+++ cookbooks/sre/switchdc/mediawiki/08-restart-parsoid.py
@@ -13,5 +13,6 @@ def main(args, spicerack):
     args = parse_args(__name__, __title__, args)
 
     logger.info('Rolling restart of Parsoid in %s and %s', args.dc_from, args.dc_to)
-    remote_hosts = spicerack.remote().query('O:parsoid')
+    # Skip wtp1043 due to T196886
+    remote_hosts = spicerack.remote().query('O:parsoid and not wtp1043.eqiad.wmnet')
     remote_hosts.run_sync('restart-parsoid', batch_size=1, batch_sleep=15.0)
