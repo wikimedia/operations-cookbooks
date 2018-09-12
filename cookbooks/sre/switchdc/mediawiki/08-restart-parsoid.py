2b7cfb157bb2db4198a2bb1a1567c5289ac961ee
Dzahn <dzahn@wikimedia.org>
2018-09-12 16:18:16 +0000

    Revert "sre.switchdc.mediawiki: parsoid skip broken host"
    This reverts commit 5c791046a9932340c9ec0ef8a14913df80492da4.
    
    Change-Id: I65a733397b1d98cdc7b87cedc4735fdf5c301491

--- cookbooks/sre/switchdc/mediawiki/08-restart-parsoid.py
+++ cookbooks/sre/switchdc/mediawiki/08-restart-parsoid.py
@@ -13,6 +13,5 @@ def main(args, spicerack):
     args = parse_args(__name__, __title__, args)
 
     logger.info('Rolling restart of Parsoid in %s and %s', args.dc_from, args.dc_to)
-    # Skip wtp1043 due to T196886
-    remote_hosts = spicerack.remote().query('O:parsoid and not wtp1043.eqiad.wmnet')
+    remote_hosts = spicerack.remote().query('O:parsoid')
     remote_hosts.run_sync('restart-parsoid', batch_size=1, batch_sleep=15.0)
