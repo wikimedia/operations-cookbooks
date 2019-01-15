fb89c6f8b134b80b1fd20f8a5ebd0e4d7f21db9e
Riccardo Coccioli <rcoccioli@wikimedia.org>
2019-01-15 15:17:59 +0100

    sre.host: add Icinga downtime cookbook
    Bug: T205886
    Change-Id: Ic6edd8aa8d5910de390516dca77761e5a1acbf39

---
+++ cookbooks/sre/hosts/downtime.py
@@ -0,0 +1,64 @@
+"""Downtime hosts and all their services in Icinga.
+
+- Optionally force a Puppet run on the Icinga host to pick up new hosts or services
+- Set Icinga downtime for the given time with a default of 4h if not specified
+
+Usage example:
+    cookbook sre.hosts.downtime --days 5 'cp1234*'
+    cookbook sre.hosts.downtime --minutes 20 cp1234.eqiad.wmnet
+    cookbook sre.hosts.downtime --minutes 20 'O:cache::upload'
+
+"""
+import argparse
+import logging
+
+from datetime import timedelta
+
+
+DEFAULT_DOWNTIME_HOURS = 4
+__title__ = 'Downtime hosts and all their services in Icinga.'
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+def argument_parser():
+    """As specified by Spicerack API."""
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('query', help='Cumin query to match the host(s) to act upon.')
+    parser.add_argument('-r', '--reason', required=True,
+                        help=('The reason for the downtime. The current username and originating host are '
+                              'automatically added.'))
+    parser.add_argument('-t', '--task-id', help='An optional task ID to refer in the downtime message (i.e. T12345).')
+    parser.add_argument('-M', '--minutes', type=int, default=0,
+                        help='For how many minutes the downtime should last. [optional, default=0]')
+    parser.add_argument('-H', '--hours', type=int, default=0,
+                        help='For how many hours the downtime should last. [optional, default=0]')
+    parser.add_argument('-D', '--days', type=int, default=0,
+                        help='For how many days the downtime should last. [optional, default=0]')
+    parser.add_argument('--force-puppet', action='store_true',
+                        help='Force a Puppet run on the Icinga host to pick up new hosts or services.')
+
+    return parser
+
+
+def post_process_args(args):
+    """Do any post-processing of the parsed arguments."""
+    if not any((args.minutes, args.hours, args.days)):
+        logger.info('No downtime length option specified, using default value of %d hours', DEFAULT_DOWNTIME_HOURS)
+        args.hours = DEFAULT_DOWNTIME_HOURS
+
+
+def run(args, spicerack):
+    """Required by Spicerack API."""
+    post_process_args(args)
+    duration = timedelta(days=args.days, hours=args.hours, minutes=args.minutes)
+    remote_hosts = spicerack.remote().query(args.query)
+    icinga = spicerack.icinga()
+    reason = spicerack.admin_reason(args.reason, task_id=args.task_id)
+
+    if args.force_puppet_run:
+        puppet = spicerack.puppet(spicerack.icinga_master_host)
+        logging.info('Forcing a Puppet run on the Icinga server')
+        puppet.run(quiet=True, attempts=30)
+
+    logging.info('Downtiming %d hosts and all their services for %s: %s', len(remote_hosts), duration, remote_hosts)
+    icinga.downtime_hosts(remote_hosts.hosts, reason, duration=duration)
