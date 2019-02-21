6b026701c59fc4bc797698e5f5cf1b5a2d523a6b
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2019-02-21 15:36:51 +0100

    elasticsearch: add cookbook to force allocation of all shards
    Change-Id: I674d12873c83508637dde2451967d0e68165a839

---
+++ cookbooks/sre/elasticsearch/force-shard-allocation.py
@@ -0,0 +1,23 @@
+"""Force allocation of all shards"""
+import argparse
+import logging
+
+from cookbooks.sre.elasticsearch import CLUSTERGROUPS
+
+__title__ = __doc__
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+def argument_parser():
+    """As specified by Spicerack API."""
+    parser = argparse.ArgumentParser(prog=__name__, description=__title__,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('clustergroup', choices=CLUSTERGROUPS, help='Name of clustergroup. One of: %(choices)s.')
+    return parser
+
+
+def run(args, spicerack):
+    """Required by Spicerack API."""
+    elasticsearch_clusters = spicerack.elasticsearch_clusters(args.clustergroup)
+
+    elasticsearch_clusters.force_allocation_of_all_unassigned_shards()
