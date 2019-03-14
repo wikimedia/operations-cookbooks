7d41039e20d98fba1c688199aab1bdca53b15e3b
Cas Rusnov <crusnov@wikimedia.org>
2019-03-14 08:59:11 -0700

    Port MakeVM to a cookbook
    * Create command line for making a new Ganeti VM,
      prompt for confirmation and then execute.
    * Print MAC address of new VM after creation.
    
    Bug: T203963
    Change-Id: Id61684a469251c83e01e1cb6ce8a4c940643df7a

---
+++ cookbooks/sre/ganeti/makevm.py
@@ -0,0 +1,100 @@
+"""Create a new Virtual Machine in Ganeti
+
+Examples:
+    Create a VM with 1 VCPU and 3 gigabytes of ram and 100 gigabytes of disk on codfw::
+
+        makevm codfw_B mytestvm.codfw.wmnet --vcpus 1 --memory 3 --disk 100
+
+"""
+
+import argparse
+import logging
+
+from spicerack.ganeti import CLUSTERS_AND_ROWS
+from spicerack.interactive import ask_confirmation
+
+__title__ = 'Create a new virtual machine in Ganeti.'
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+CREATE_COMMAND = (
+    'gnt-instance add -t drbd -I hail'
+    ' --net 0:link={link}'
+    ' --hypervisor-parameters=kvm:boot_order=network'
+    ' -o bootstrap+default'
+    ' --no-install'
+    ' -g row_{row}'
+    ' -B vcpus={vcpus},memory={memory}g'
+    ' --disk 0:size={disk}g'
+    ' {fqdn}'
+)
+
+
+def argument_parser():
+    """Parse command-line arguments for this module per spicerack API."""
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    # build option list from cluster definitions
+    clusters_and_rows = [cluster + '_' + row for cluster, row in CLUSTERS_AND_ROWS.items()]
+
+    parser.add_argument(
+        'cluster_and_row',
+        choices=clusters_and_rows,
+        help='Ganeti cluster identifier %(choices)s',
+        type=lambda s: s.split('_', 1),
+    )
+    parser.add_argument('fqdn', 'The FQDN for the VM.')
+    parser.add_argument(
+        '--vcpus', help='The number of virtual CPUs to assign to VM (default: %(default)s).', type=int, default=1
+    )
+    parser.add_argument(
+        '--memory',
+        help='The number of gigabytes of ram to allocate to the VM (default: %(default)s).',
+        type=int,
+        default=1,
+    )
+    parser.add_argument(
+        '--disk', help='Number of gigabytes of disk to allocate to the VM (default: %(default)s).', type=int, default=10
+    )
+    parser.add_argument(
+        '--public', help='Specify that a public IP address is required (default: %(default)s).', action='store_true'
+    )
+    return parser
+
+
+def run(args, spicerack):
+    """Create a new Ganeti VM as specified."""
+    # grab the cluster master from RAPI
+    cluster, row = args.cluster_and_row
+    ganeti = spicerack.ganeti()
+    cluster_fqdn = ganeti.rapi(cluster).master
+    ganeti_host = spicerack.remote().query(cluster_fqdn)
+
+    # link type for public/private IP
+    link = 'private'
+    if args.public:
+        link = 'public'
+
+    # Create command line
+    command = CREATE_COMMAND.format(
+        link=link, row=row, vcpus=args.vcpus, memory=args.memory, disk=args.disk, fqdn=args.fqdn
+    )
+
+    logger.info(
+        'Creating new VM named %s in %s with row=%s vcpu=%d memory=%d gigabytes disk=%d gigabytes',
+        args.fqdn,
+        cluster,
+        row,
+        args.vcpus,
+        args.memory,
+        args.disk,
+    )
+
+    ask_confirmation('Is this correct?')
+
+    results = ganeti_host.run_sync(command)
+
+    for _, output in results:
+        logger.info(output.message().decode())
+
+    # get MAC address of instance
+    logger.info('instance %s created with MAC %s', args.fqdn, ganeti.rapi(cluster).fetch_instance_mac(args.fqdn))
