0b314f80cde414d65a89af5c3943345fd2499cac
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2019-03-29 16:22:03 +0100

    Cookbook to reset frozen writes on elasticsearch / cirrus.
    Bug: T219638
    Change-Id: I0160eed91c7df7515017318ad4112049bbe94b7f

---
+++ cookbooks/sre/elasticsearch/force-unfreeze.py
@@ -0,0 +1,31 @@
+"""Unfreeze writes forcefully in case they are stuck to frozen."""
+
+import argparse
+import logging
+
+from cookbooks.sre.elasticsearch import CLUSTERGROUPS
+
+__title__ = __doc__
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+def argument_parser():
+    """As specified by Spicerack API."""
+    parser = argparse.ArgumentParser(prog=__name__, description=__title__,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('clustergroup', choices=CLUSTERGROUPS, help='Name of clustergroup. One of: %(choices)s.')
+    parser.add_argument('admin_reason', help='Administrative Reason')
+    parser.add_argument('--task-id', help='task_id for the change')
+    return parser
+
+
+def run(args, spicerack):
+    """Required by Spicerack API."""
+    elasticsearch_clusters = spicerack.elasticsearch_clusters(args.clustergroup)
+    reason = spicerack.admin_reason(args.admin_reason, task_id=args.task_id)
+
+    # If frozen state is in an unstable state, freezing and thawing writes can
+    # restore a normal situation. See incident report below for details.
+    # https://wikitech.wikimedia.org/wiki/Incident_documentation/20190327-elasticsearch
+    with elasticsearch_clusters.frozen_writes(reason):
+        logger.info('Thawing writes again!')
