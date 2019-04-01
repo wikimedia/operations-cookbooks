3d9a6f78e938510259a33888cae1299aed312d3d
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2019-04-01 17:28:30 +0200

    elasticsearch: add cookbook to reboot all nodes in a cluster
    Change-Id: I5d78a99a06c61fd2f706a95472fffe10e305a9c5

---
+++ cookbooks/sre/elasticsearch/rolling-reboot.py
@@ -0,0 +1,26 @@
+"""Rolling reboot of elasticsearch servers"""
+import logging
+
+from cookbooks.sre.elasticsearch import argument_parser_base, post_process_args, execute_on_clusters
+
+__title__ = __doc__
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+def argument_parser():
+    """As specified by Spicerack API."""
+    return argument_parser_base(__name__, __title__)
+
+
+def run(args, spicerack):
+    """Required by Spicerack API."""
+    post_process_args(args)
+    icinga = spicerack.icinga()
+    elasticsearch_clusters = spicerack.elasticsearch_clusters(args.clustergroup)
+    reason = spicerack.admin_reason(args.admin_reason, task_id=args.task_id)
+
+    execute_on_clusters(
+        elasticsearch_clusters, icinga, reason, spicerack, args.nodes_per_run,
+        args.clustergroup, args.start_datetime, args.with_lvs, args.wait_for_green,
+        lambda nodes: nodes.get_remote_hosts().reboot(batch_size=args.nodes_per_run)
+    )
