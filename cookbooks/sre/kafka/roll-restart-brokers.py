57e5fc91601350c4e6fa4b0fdac8ed296642494d
Luca Toscano <ltoscano@wikimedia.org>
2019-07-30 07:52:10 +0200

    sre.kafka.roll-restart-brokers.py: improvements to the procedure
    This change improves the kafka broker restart sequence, forcing
    a preferred-replica-election after the restart to avoid all corner
    cases in which partition leaders might end up not be balanced by the
    time that another broker is restarted. The sleep time after the
    restart is also increased, 10 mins has been proven too tight when
    restarting the Jumbo brokers.
    
    Bug: T229003
    Change-Id: I14fc1a2707c08ef4de8d6d989c62ebf9026c23d8

--- cookbooks/sre/kafka/roll-restart-brokers.py
+++ cookbooks/sre/kafka/roll-restart-brokers.py
@@ -60,8 +60,11 @@ def argument_parser():
     parser.add_argument('cluster', help='The name of the Kafka cluster to work on.',
                         choices=['main-eqiad', 'jumbo', 'main-codfw',
                                  'logging-eqiad', 'logging-codfw'])
-    parser.add_argument('--batch-sleep-seconds', type=float, default=600.0,
+    parser.add_argument('--batch-sleep-seconds', type=float, default=300.0,
                         help="Seconds to sleep between each broker restart.")
+    parser.add_argument('--sleep-before-pref-replica-election', type=int, default=900,
+                        help="Seconds to sleep between a broker restart and "
+                             "the kafka preferred-replica-election execution.")
     return parser
 
 
@@ -81,10 +84,28 @@ def run(args, spicerack):
     logger.info('Checking that all Kafka brokers are reported up by their systemd unit status.')
     kafka_brokers.run_sync('systemctl status kafka')
 
-    with icinga.hosts_downtimed(kafka_brokers.hosts, reason,
-                                duration=timedelta(minutes=120)):
+    if args.sleep_before_pref_replica_election < 900:
+        ask_confirmation(
+            'The sleep time between a broker restart and kafka preferred-replica-election '
+            'is less than 900 seconds. The broker needs some time to recover after a restart. '
+            'Are you sure?')
 
-        kafka_brokers.run_sync(
-            'systemctl restart kafka', batch_size=1, batch_sleep=args.batch_sleep_seconds)
+    with icinga.hosts_downtimed(kafka_brokers.hosts, reason,
+                                duration=timedelta(minutes=240)):
+
+        # The idea is to:
+        # 1) restart the kafka broker
+        # 2) wait 900s to make sure that any unbalanced/under-replicated/etc.. partition recovers.
+        # 3) force a prefered-replica-election to make sure that partition leaders are balanced
+        #    before the next broker is restarted. This is not strictly needed since they should
+        #    auto-rebalance, but there are rare use cases in which it might not happen.
+        # 4) sleep for args.batch_sleep_seconds before the next restart.
+        commands = [
+          'systemctl restart kafka',
+          'sleep ' + str(args.sleep_before_pref_replica_election),
+          'kafka preferred-replica-election'
+        ]
+        kafka_brokers.run_async(
+          *commands, batch_size=1, batch_sleep=args.batch_sleep_seconds)
 
     logger.info('All Kafka broker restarts completed!')
