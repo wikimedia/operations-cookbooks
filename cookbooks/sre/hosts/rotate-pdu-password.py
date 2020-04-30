7fa8ea13b2165271cff7eb7a9a3c09939890f3b3
John Bond <jbond@wikimedia.org>
2020-04-30 12:14:31 +0200

    cookbooks sre.hosts.rotate-pdu-password: refactor
     * use request.Session
     * make use of Exception
    
    Bug: T246890
    Change-Id: I384d8dfcd67adb66e0f4cff1c078e71d1d7aa222

--- cookbooks/sre/hosts/rotate-pdu-password.py
+++ cookbooks/sre/hosts/rotate-pdu-password.py
@@ -6,24 +6,36 @@
 - So try --dry-run first 😉
 
 Usage example:
-    cookbook sre.hosts.rotate-pdu-password --username MrFoo --check_default 'ps1-b5-eqiad.mgmt.eqiad.wmnet'
+    cookbook sre.hosts.rotate-pdu-password --username MrFoo 'ps1-b5-eqiad.mgmt.eqiad.wmnet'
     cookbook sre.hosts.rotate-pdu-password all
+    cookbook sre.hosts.rotate-pdu-password all --check_default
 """
 
-import argparse
-import getpass
 import logging
-import requests
-import urllib3
+
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+from requests import get, Session
+from requests.exceptions import HTTPError
+
+from spicerack.interactive import ensure_shell_is_durable, get_secret
 
 __title__ = 'Update Sentry PDUs 🔌 passwords'
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
-urllib3.disable_warnings()  # Mute insecure TLS warning (as PDUs certs are self signed)
+
+
+class PasswordResetError(Exception):
+    """Raised if password reset fails"""
+
+
+class GetVersionError(Exception):
+    """Raised if there is an issue getting PDU version"""
 
 
 def argument_parser():
     """As specified by Spicerack API."""
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = ArgumentParser(
+        description=__doc__, formatter_class=RawDescriptionHelpFormatter)
     parser.add_argument('--username', help='Username (default: %(default)s)', default='root')
     parser.add_argument('--check_default', help='Check for default user',
                         action='store_true')
@@ -34,141 +46,134 @@ def argument_parser():
 
 def run(args, spicerack):  # noqa: MC0001
     """Required by Spicerack API."""
-    current_password = getpass.getpass("Current password:")
-    if not current_password:
-        logger.error('Can\'t be empty 👎')
-        return 1
-    new_password = getpass.getpass("New password:")
-    new_password_bis = getpass.getpass("Again, just to be sure:")
-    if new_password != new_password_bis:
-        logger.error('They don\'t match 👎')
-        return 1
-
-    pdus = []
+    ensure_shell_is_durable()
+    session = Session()
+    session.verify = False
     return_code = 0
-    if args.query == 'all':
-        logger.info('Query PDUs list from PuppetDB')
-        puppet_db_fqdn = spicerack.remote().query('A:puppetdb').hosts[0]
-        three_phases_url = 'https://{puppet_db_fqdn}/pdb/query/v4/resources/Facilities::Monitor_pdu_3phase'.format(
-                            puppet_db_fqdn=puppet_db_fqdn)
-        monophase_url = 'https://{puppet_db_fqdn}/pdb/query/v4/resources/Facilities::Monitor_pdu_1phase'.format(
-                         puppet_db_fqdn=puppet_db_fqdn)
-        three_phases = requests.get(three_phases_url).json()
-        monophase = requests.get(monophase_url).json()
-        for pdu in three_phases + monophase:
-            pdu_fqdn = '{hostname}.mgmt.{site}.wmnet'.format(hostname=pdu['title'], site=pdu['parameters']['site'])
-            if pdu_fqdn not in pdus:
-                pdus.append(pdu_fqdn)
-    else:
-        pdus.append(args.query)
+    current_password = get_secret('Current password')
+    new_password = get_secret("New password", confirm=True)
 
-    for pdu_fqdn in pdus:
+    session.auth = (args.username, current_password)
+
+    if args.query == 'all':
+        netbox = spicerack.netbox()
+        devices = netbox.api.dcim.devices.filter(role='pdu')
+        pdus = [str(device.primary_ip).split('/')[0] for device in devices
+                if device.primary_ip is not None]
+    else:
+        pdus = set(args.query)
+
+    for pdu in pdus:
         try:
             if not spicerack.dry_run:
-                if change_password(pdu_fqdn, args.username, current_password, new_password) == 1:
-                    return_code = 1
+                change_password(pdu, session, new_password)
             else:
-                logger.info('{pdu_fqdn}: Dry run, not trying.'.format(pdu_fqdn=pdu_fqdn))
+                logger.info('%s: Dry run, not trying.', pdu)
             if args.check_default:
-                if check_default(pdu_fqdn) == 1:
+                if check_default(pdu):
+                    # TODO: delete default user
                     return_code = 1
-        except Exception as e:
-            logger.error('{pdu_fqdn}: Something went wrong: {error}'.format(pdu_fqdn=pdu_fqdn, error=str(e)))
-
+        except GetVersionError as error:
+            logger.error('%s: Failed to get PDU version: %s', pdu, str(error))
+            return_code = 1
+        except PasswordResetError as error:
+            logger.error('%s: Failed to reset password: %s', pdu, str(error))
+            return_code = 1
+        except Exception as error:
+            logger.error('%s: Something went wrong: %s', pdu, str(error))
     return return_code
 
 
-def get_version(pdu_fqdn, username, password):
+def get_version(pdu, session):
     """Check the firmware version and returns the matching Sentry version
 
+    Arguments:
+        pdu (str): the pdu fqdn
+        session (requests.Session): A configured request session
+
     Returns:
-        int: Sentry version
-        or None
+        int: the version number
+
+    Raises:
+        GetVersionError
 
     """
-    response_version_check = requests.get("https://{fqdn}/chngpswd.html".format(fqdn=pdu_fqdn),
-                                          verify=False,  # nosec
-                                          auth=(username, password))
-    if response_version_check.status_code == 200:
-        if 'v7' in response_version_check.headers['Server']:
-            return 3
-            logger.debug('{pdu_fqdn}: Sentry 3 detected'.format(pdu_fqdn=pdu_fqdn))
-        elif 'v8' in response_version_check.headers['Server']:
-            logger.debug('{pdu_fqdn}: Sentry 4 detected'.format(pdu_fqdn=pdu_fqdn))
-            return 4
-        else:
-            logger.error('{pdu_fqdn}: Unknown Sentry version'.format(pdu_fqdn=pdu_fqdn))
-            return 0
-    else:
-        logger.error("{pdu_fqdn}: Error {status_code} while trying to check the version.".format(
-            status_code=response_version_check.status_code, pdu_fqdn=pdu_fqdn))
+    try:
+        response = session.get("https://{}/chngpswd.html".format(pdu))
+        response.raise_for_status()
+    except HTTPError as err:
+        raise GetVersionError("{}: Error {} while trying to check the version: {}".format(
+            pdu, response.status_code, err))
+    if 'v7' in response.headers['Server']:
+        logger.debug('%s: Sentry 3 detected', pdu)
+        return 3
+    if 'v8' in response.headers['Server']:
+        logger.debug('%s: Sentry 4 detected', pdu)
+        return 4
+    raise GetVersionError('{}: Unknown Sentry version'.format(pdu))
 
 
-def change_password(pdu_fqdn, username, current_password, new_password):
+def change_password(pdu, session, new_password):
     """Change the password
 
-    Returns:
-        1 if any issue.
-        0 if all good.
+    Arguments:
+        pdu (str): the pdu fqdn
+        session (requests.Session): A configured request session
+        new_password (str): the new password to use
+
+    Raises:
+        PasswordResetError
 
     """
-    version = get_version(pdu_fqdn, username, current_password)
-    if version == 3:
-        payload = {'Current_Password': current_password,
-                   'New_Password': new_password,
-                   'New_Password_Verify': new_password}
-    elif version == 4:
-        payload = {'FormButton': 'Apply',
-                   'UPWC': current_password,
-                   'UPW': new_password,
-                   'UPWV': new_password}
-    else:
-        return 1
-
-    # Send auth to any page to get the cookies settings
-    request_cookies = requests.get("https://{fqdn}/chngpswd.html".format(fqdn=pdu_fqdn),
-                                   verify=False,  # nosec
-                                   auth=(username, current_password))
+    payload = {
+        3: {
+            'Current_Password': session.auth[1],
+            'New_Password': new_password,
+            'New_Password_Verify': new_password
+        },
+        4: {
+            'FormButton': 'Apply',
+            'UPWC': session.auth[1],
+            'UPW': new_password,
+            'UPWV': new_password
+        }
+    }.get(get_version(pdu, session))
 
     # Then change the password
-    response_change_pw = requests.post("https://{fqdn}/Forms/chngpswd_1".format(fqdn=pdu_fqdn),
-                                       data=payload,
-                                       verify=False,  # nosec
-                                       cookies=request_cookies.cookies,
-                                       auth=(username, current_password))  # nosec
+    try:
+        response = session.post("https://{}/Forms/chngpswd_1".format(pdu), data=payload)
+        response.raise_for_status()
+    except HTTPError as err:
+        raise PasswordResetError("{}: Error {} while trying to change the password: {}".format(
+            pdu, response.status_code, err))
 
-    if response_change_pw.status_code == 200:
-        response_check_success = requests.get("https://{fqdn}/chngpswd.html".format(
-                                              fqdn=pdu_fqdn),
-                                              verify=False,  # nosec
-                                              auth=(username, new_password))
-        if response_check_success.status_code == 200:
-            logger.info('{pdu_fqdn}: Password updated successfully 😌'.format(pdu_fqdn=pdu_fqdn))
-            return 0
-        else:
-            logger.error('{pdu_fqdn}: Error {status_code}. New password not working, \
-the change probably failed'.format(status_code=response_check_success.status_code, pdu_fqdn=pdu_fqdn))
-            return 1
-    else:
-        logger.error("{pdu_fqdn}: Error {status_code} while trying to change the password.".format(
-            status_code=response_change_pw.status_code, pdu_fqdn=pdu_fqdn))
-        return 1
+    session.auth[1] = new_password
+    session.cookies.clear()
+    try:
+        response = session.get("https://{}/chngpswd.html".format(pdu))
+        response.raise_for_status()
+    except HTTPError as err:
+        raise PasswordResetError(
+            '{}: Error {}. New password not working, the change probably failed:\n{}'.format(
+                pdu, response.status_code, err))
+    logger.info('%s: Password updated successfully 😌', pdu)
 
 
-def check_default(pdu_fqdn):
+def check_default(pdu):
     """Checks if the default password is set on the device
 
+    Arguments:
+        pdu (str): the pdu
+
     Returns:
-        1 if default user found.
-        0 if all good.
+        bool: indicating if the default password is in use
 
     """
-    response = requests.get("https://{fqdn}/chngpswd.html".format(fqdn=pdu_fqdn),
-                            verify=False,  # nosec
-                            auth=('admn', 'admn'))
-    if response.status_code == 200:
-        logger.warning('{pdu_fqdn}: Default user found 😞'.format(pdu_fqdn=pdu_fqdn))
-        return 1
-    else:
-        logger.info('{pdu_fqdn}: No default user 👍'.format(pdu_fqdn=pdu_fqdn))
-        return 0
+    response = get("https://{}/chngpswd.html".format(pdu),
+                   verify=False,  # nosec
+                   auth=('admn', 'admn'))
+    if response.ok:
+        logger.warning('%s: Default user found 😞', pdu)
+        return True
+    logger.info('%s: No default user 👍', pdu)
+    return False
