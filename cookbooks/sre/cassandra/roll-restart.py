b99a51a7dc781975f839e0585ecc106369ce4afe
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-06-04 12:16:00 +0200

    Drop maps from supported clusters
    There's no multi instance setup in the Cassandra setup on maps.
    
    Change-Id: I4e4657a75cedf948961e7e8249138a46e66fba91

--- cookbooks/sre/cassandra/roll-restart.py
+++ cookbooks/sre/cassandra/roll-restart.py
@@ -18,7 +18,7 @@ def argument_parser():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=ArgparseFormatter)
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('cluster', nargs='?',
-                       choices=['aqs', 'restbase-eqiad', 'restbase-dev', 'maps', 'sessionstore',
+                       choices=['aqs', 'restbase-eqiad', 'restbase-dev', 'sessionstore',
                                 'restbase-codfw'],
                        help=('The name of the Cassandra cluster to work on. This refers to ',
                              'a Cumin alias. Alternatively you can pass an alternative Cumin ',
