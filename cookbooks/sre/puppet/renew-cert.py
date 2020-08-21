a3c00f5b3fadcc01144f17fcfc16f4699d8f9d58
John Bond <jbond@wikimedia.org>
2020-08-21 12:34:09 +0200

    cookbook sre.puppet.renew-cert: add cookbook to renew a puppet cert
    This cookbook preforms the following steps
     * puppet cert clean the old certificate on the puppet master
     * delete the old certificate on the host
     * run puppet to generate a new certificate and the host
     * validate the puppet master see's the new certificate on the puppet master
     * sign the new certificate on the puppet master
     * run puppet on the host to ensure everything works as expected
    
    This may be better placed in sre.hosts.renew-puppet-cert?
    
    Bug: T260110
    Change-Id: Ic687d279471799d801c6d7ee1a52bea1aafca7e1

---
+++ cookbooks/sre/puppet/renew-cert.py
@@ -0,0 +1,52 @@
+r"""Renew the puppet certificate of a single host
+
+ * puppet cert clean the old certificate on the puppet master
+ * delete the old certificate on the host
+ * run puppet to generate a new certificate and the host
+ * validate the puppet master see's the new certificate on the puppet master
+ * sign the new certificate on the puppet master
+ * run puppet on the host to ensure everything works as expected
+
+Usage example:
+    cookbook sre.hosts.renew-cert sretest1001.eqiad.wmnet
+"""
+
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from datetime import timedelta
+from logging import getLogger
+
+
+__title__ = 'Renew the puppet certificate of a single host'
+logger = getLogger(__name__)  # pylint: disable=invalid-name
+
+
+def argument_parser():
+    """Parse arguments"""
+    parser = ArgumentParser(description=__doc__, formatter_class=RawDescriptionHelpFormatter)
+    parser.add_argument('host', help='A single host whose puppet certificate should be renewed')
+    return parser
+
+
+def run(args, spicerack):
+    """Renew the certificate"""
+    remote_host = spicerack.remote().query(args.host)
+
+    if not remote_host:
+        logger.error('Specified server not found, bailing out')
+        return 1
+
+    if len(remote_host) != 1:
+        logger.error('Only a single server can be rebooted')
+        return 1
+
+    icinga = spicerack.icinga()
+    puppet = spicerack.puppet(remote_host)
+    puppet_master = spicerack.puppet_master()
+    reason = spicerack.admin_reason('Renew puppet certificate')
+    with icinga.hosts_downtimed(remote_host.hosts, reason, duration=timedelta(minutes=20)):
+        puppet_master.destroy(remote_host)
+        puppet.disable(reason)
+        fingerprints = puppet.regenerate_certificate()
+        puppet_master.wait_for_csr(remote_host)
+        puppet_master.sign(remote_host, fingerprints[remote_host])
+        puppet.run(enable_reason=reason, quiet=True)
