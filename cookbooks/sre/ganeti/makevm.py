5a6cca68be54f79ef5091bf8de6d8f007236011f
Riccardo Coccioli <rcoccioli@wikimedia.org>
2020-09-01 11:26:10 +0200

    sre.ganeti.makevm: adapt to Netbox DNS automation
    * Pre-allocated the IPs with their DNS names
    * Update the generated DNS records before creating the VM
    * Sync the VMs to Netbox after VM creation
    * Update Netbox after the VM creation
    
    Bug: T258729
    Change-Id: I6bdfc6cedf389b3188cb6ee00de13538e304e64b

--- cookbooks/sre/ganeti/makevm.py
+++ cookbooks/sre/ganeti/makevm.py
@@ -1,5 +1,11 @@
 """Create a new Virtual Machine in Ganeti
 
+* Pre-allocate the primary IPs and set their DNS name
+* Update the DNS records
+* Create the VM on Ganeti
+* Force a sync of Ganeti VMs to Netbox in the same DC
+* Update Netbox attaching the pre-allocated IPs to the host's primary interface
+
 Examples:
     Create a Ganeti VM vmname.codfw.wmnet in the codfw Ganeti cluster on row B with 1 vCPUs, 3GB of RAM, 100GB of disk
     in the private network:
@@ -9,17 +15,22 @@ Examples:
 """
 
 import argparse
+import ipaddress
 import logging
 
-from spicerack.dns import Dns, DnsNotFound
+from spicerack.constants import CORE_DATACENTERS
+from spicerack.decorators import retry
 from spicerack.ganeti import CLUSTERS_AND_ROWS, INSTANCE_LINKS
 from spicerack.interactive import ask_confirmation, ensure_shell_is_durable
 
 from cookbooks import ArgparseFormatter
+from cookbooks.sre.dns.netbox import argument_parser as dns_netbox_argparse, run as dns_netbox_run
 
 
 __title__ = 'Create a new virtual machine in Ganeti.'
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+MIGRATED_PRIMARY_SITES = ()
+PRIMARY_INTERFACE_NAME = '##PRIMARY##'
 
 
 def _get_locations():
@@ -28,10 +39,10 @@ def _get_locations():
     for cluster, rows in CLUSTERS_AND_ROWS.items():
         dc = cluster.split('.')[2]
         if len(rows) == 1:
-            locations[dc] = (cluster, rows[0])
+            locations[dc] = (cluster, rows[0], dc)
         else:
             for row in rows:
-                locations['{dc}_{row}'.format(dc=dc, row=row)] = (cluster, row)
+                locations['{dc}_{row}'.format(dc=dc, row=row)] = (cluster, row, dc)
 
     return locations
 
@@ -46,20 +57,9 @@ def argument_parser():
 
         return value
 
-    def valid_fqdn(param):
-        """Type validator for argparse that verify the existence of the DNS records for the VM."""
-        resolver = Dns()
-        try:
-            ips = resolver.resolve_ipv4(param)
-            for ip in ips:
-                resolver.resolve_ptr(ip)
-        except DnsNotFound as e:
-            raise argparse.ArgumentTypeError('missing DNS records for FQDN {fqdn}: {e}'.format(fqdn=param, e=e))
-
-        return param
-
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=ArgparseFormatter)
 
+    parser.add_argument('--skip-v6', action='store_true', help='To skip the generation of the IPv6 DNS record.')
     parser.add_argument(
         '--vcpus', type=positive_int, default=1, help='The number of virtual CPUs to assign to the VM.')
     parser.add_argument(
@@ -69,8 +69,8 @@ def argument_parser():
     parser.add_argument('--network', choices=INSTANCE_LINKS, default='private',
                         help='Specify the type of network to assign to the VM.')
     parser.add_argument('location', choices=sorted(_get_locations().keys()),
-                        help='The datacenter and row (only for multi-row clusters) where to create the VM')
-    parser.add_argument('fqdn', type=valid_fqdn, help='The FQDN for the VM. The DNS records must exist.')
+                        help='The datacenter and row (only for multi-row clusters) where to create the VM.')
+    parser.add_argument('fqdn', help='The FQDN for the VM.')
 
     return parser
 
@@ -78,7 +78,66 @@ def argument_parser():
 def run(args, spicerack):
     """Create a new Ganeti VM as specified."""
     ensure_shell_is_durable()
-    args.cluster, args.row = _get_locations()[args.location]  # Inject cluster and row in the args object.
+    args.cluster, args.row, args.dc = _get_locations()[args.location]  # Inject cluster, row and dc in the args object.
+    hostname = args.fqdn.split('.')[0]
+
+    # Pre-allocate IPs
+    netbox = spicerack.netbox(read_write=True)
+    if args.dc in CORE_DATACENTERS:
+        vlan_name = '{a.network}1-{a.row}-{a.dc}'.format(a=args)
+    else:
+        vlan_name = '{a.network}1-{a.dc}'.format(a=args)
+
+    vlan = netbox.api.ipam.vlans.get(name=vlan_name, status='active')
+    prefix_v4 = netbox.api.ipam.prefixes.get(vlan_id=vlan.id, family=4)
+    prefix_v6 = netbox.api.ipam.prefixes.get(vlan_id=vlan.id, family=6)
+    ip_v4_data = prefix_v4.available_ips.create()
+    logger.info('Allocated IPv4 %s', ip_v4_data['address'])
+    ip_v4 = netbox.api.ipam.ip_addresses.get(address=ip_v4_data['address'])
+    ip_v4.dns_name = args.fqdn
+    if not ip_v4.save():
+        logger.error('Failed to save DNS name for IP %s on Netbox', ip_v4)
+        return 1
+
+    logger.info('Set DNS name of IP %s to %s', ip_v4, args.fqdn)
+
+    # Generate the IPv6 address embedding the IPv4 address, for example from an IPv4 address 10.0.0.1 and an
+    # IPv6 prefix 2001:db8:3c4d:15::/64 the mapped IPv6 address 2001:db8:3c4d:15:10:0:0:1/64 is generated.
+    prefix_v6_base, prefix_v6_mask = str(prefix_v6).split("/")
+    mapped_v4 = str(ip_v4).split('/')[0].replace(".", ":")
+    ipv6_address = '{prefix}:{mapped}/{mask}'.format(
+        prefix=prefix_v6_base.rstrip(':'), mapped=mapped_v4, mask=prefix_v6_mask)
+    if args.skip_v6:
+        dns_name_v6 = ''
+    else:
+        dns_name_v6 = args.fqdn
+    ip_v6 = netbox.api.ipam.ip_addresses.create(address=ipv6_address, status='active', dns_name=dns_name_v6)
+    logger.info('Allocated IPv6 %s with DNS name %s', ip_v6, dns_name_v6)
+
+    # Run the sre.dns.netbox cookbook to generate the DNS records
+    if args.dc in MIGRATED_PRIMARY_SITES:
+        dns_netbox_args = dns_netbox_argparse().parse_args(['Created records for VM {vm}'.format(vm=args.fqdn)])
+        dns_netbox_run(dns_netbox_args, spicerack)
+    else:
+        ip_v4_address = ipaddress.ip_interface(ip_v4).ip
+        ip_v6_address = ipaddress.ip_interface(ip_v6).ip
+        if args.skip_v6:
+            ip_v6_message = ''
+        else:
+            ip_v6_message = '\n    IPv6: {ip}\n    PTRv6: {ptr}\n    DNSv6: {dns}'.format(
+                ip=ip_v6_address, ptr=ip_v6_address.reverse_pointer, dns=dns_name_v6)
+
+        logger.warning('DC %s has not yet been migrated for primary records. Manual commit in the operations/dns '
+                       'repository is required. See '
+                       'https://wikitech.wikimedia.org/wiki/Server_Lifecycle/DNS_Transition'
+                       'Please make a patch to the DNS repository for the following data:'
+                       '\n\n    IPv4:  %s\n    PTRv4: %s'
+                       '\n    DNSv4: %s%s',
+                       args.dc, ip_v4_address, ip_v4_address.reverse_pointer, args.fqdn, ip_v6_message)
+
+        ask_confirmation('Proceed only when the DNS patch has been merged and deployed')
+
+    # Create the VM
     ganeti = spicerack.ganeti()
     instance = ganeti.instance(args.fqdn, cluster=args.cluster)
 
@@ -94,3 +153,42 @@ def run(args, spicerack):
     else:
         mac = ganeti.rapi(args.cluster).fetch_instance_mac(args.fqdn)
         logger.info('MAC address for %s is: %s', args.fqdn, mac)
+
+    # Force a run of the ganeti sync
+    logger.info('Syncing VMs in DC %s to Netbox', args.dc)
+    spicerack.netbox_master_host.run_sync('systemctl start netbox_ganeti_{dc}_sync.service'.format(dc=args.dc))
+
+    # Get the synced VM
+    @retry(tries=20, backoff_mode='linear', exceptions=(RuntimeError,))
+    def get_vm(netbox):
+        vm = netbox.api.virtualization.virtual_machines.get(name=hostname)
+        if not vm:
+            raise RuntimeError
+
+        return vm
+
+    # Update Netbox
+    vm = get_vm(netbox)
+    iface = netbox.api.virtualization.interfaces.create(
+        virtual_machine=vm.id, name=PRIMARY_INTERFACE_NAME, type='virtual')
+    logger.info('Created interface %s on VM %s', PRIMARY_INTERFACE_NAME, vm)
+
+    ip_v4.interface = iface
+    if not ip_v4.save():
+        logger.error('Failed to attach IPv4 %s to interface %s', ip_v4, iface)
+        return 1
+
+    ip_v6.interface = iface
+    if not ip_v6.save():
+        logger.error('Failed to attach IPv6 %s to interface %s', ip_v6, iface)
+        return 1
+
+    vm.primary_ip4 = ip_v4
+    vm.primary_ip6 = ip_v6
+    if not vm.save():
+        logger.error('Failed to set primary IPv4/6 to VM %s', vm)
+        return 1
+
+    logger.info('Attached IPv4 %s and IPv6 %s to VM %s and marked as primary IPs', ip_v4, ip_v6, vm)
+
+    # TODO: run the Netbox import script for the VM after the first Puppet run to import all interfaces
