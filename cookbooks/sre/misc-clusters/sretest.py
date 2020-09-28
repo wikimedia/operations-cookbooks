80b0592342b42632e8cfd3e11022ae396f6b3253
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2020-09-28 13:40:55 +0200

    reboot-groups
    Create a common framework for clustered reboots with an example for
    a specialised service cookbook derived based on it.
    
    Change-Id: I008c553051f8c5cbf1e813b8ebb7e5bb5bfd7b64

---
+++ cookbooks/sre/misc-clusters/sretest.py
@@ -0,0 +1,46 @@
+"""sretest reboot cookbook
+
+Usage example:
+    cookbook sre.misc-clusters.sretest
+
+"""
+
+import logging
+from spicerack.remote import RemoteExecutionError
+from collections import namedtuple
+from cookbooks.sre.hosts.reboot_groups import argument_parser_reboot_groups, reboot_group
+
+ScriptReturn = namedtuple('ScriptReturn', 'returncode output')
+
+__title__ = 'SREtest reboot cookbook'
+batch_size = 1
+allowed_aliases = ['sretest']
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+def pre_action_example(hosts):
+    """Run pre boot script"""
+    actions = ['echo "Nothing really happens"', 'echo "Really"']
+
+    try:
+        hosts.run_sync(*actions)
+    except RemoteExecutionError as e:
+        logger.error("Failed to run pre reboot script:")
+        logger.error(e)
+        return ScriptReturn(1, e)
+
+    return ScriptReturn(0, "")
+
+
+def argument_parser():
+    """As specified by Spicerack API."""
+    return argument_parser_reboot_groups(__name__, batch_size)
+
+
+def run(args, spicerack):
+    """Required by Spicerack API."""
+    logger.info('Running reboot cookbook for %s', __title__)
+
+    return reboot_group(spicerack, args.query, args.alias, allowed_aliases, args.batchsize,
+                        [pre_action_example], None)
