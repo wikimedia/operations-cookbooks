1039cb45d6f9307dd54f6367ca88c4d2e924dba7
Riccardo Coccioli <rcoccioli@wikimedia.org>
2020-10-11 16:46:02 +0200

    sre.hosts.downtime: convert to class API
    * Convert the cookbook to the new class-based Spicerack API.
    * Perform all preliminary actions in the DonwtimeRunner's constructor so
      that any failure will be catched before the actual start of the
      cookbook and prevent any !log-spam.
    * Set a runtime_description to be used in IRC !log messages that
      includes the duration and the number of hosts affected, listing the
      actual hosts if there are just a few (limit set to 5).
    
    Bug: T221212
    Depends-On: I7b4f305d8718bf544974d0ea8a6c0df8eb5936c6
    Change-Id: I97b48851c3e33cea6f34439d3036ff0cba11eac7

--- cookbooks/sre/hosts/downtime.py
+++ cookbooks/sre/hosts/downtime.py
@@ -1,73 +1,112 @@
-"""Downtime hosts and all their services in Icinga.
-
-- Optionally force a Puppet run on the Icinga host to pick up new hosts or services
-- Set Icinga downtime for the given time with a default of 4h if not specified
-
-Usage example:
-    cookbook sre.hosts.downtime --days 5 'cp1234*'
-    cookbook sre.hosts.downtime --minutes 20 cp1234.eqiad.wmnet
-    cookbook sre.hosts.downtime --minutes 20 'O:cache::upload'
-
-"""
+"""Downtime hosts and all their services in Icinga."""
 import argparse
 import logging
 
 from datetime import timedelta
 
+from spicerack.cookbook import CookbookBase, CookbookRunnerBase
+
 from cookbooks.sre import PHABRICATOR_BOT_CONFIG_FILE
 
 
-DEFAULT_DOWNTIME_HOURS = 4
-__title__ = 'Downtime hosts and all their services in Icinga.'
 logger = logging.getLogger(__name__)
 
 
-def argument_parser():
-    """As specified by Spicerack API."""
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('query', help='Cumin query to match the host(s) to act upon.')
-    parser.add_argument('-r', '--reason', required=True,
-                        help=('The reason for the downtime. The current username and originating host are '
-                              'automatically added.'))
-    parser.add_argument('-t', '--task-id', help='An optional task ID to refer in the downtime message (i.e. T12345).')
-    parser.add_argument('-M', '--minutes', type=int, default=0,
-                        help='For how many minutes the downtime should last. [optional, default=0]')
-    parser.add_argument('-H', '--hours', type=int, default=0,
-                        help='For how many hours the downtime should last. [optional, default=0]')
-    parser.add_argument('-D', '--days', type=int, default=0,
-                        help='For how many days the downtime should last. [optional, default=0]')
-    parser.add_argument('--force-puppet', action='store_true',
-                        help='Force a Puppet run on the Icinga host to pick up new hosts or services.')
+class Downtime(CookbookBase):
+    """Downtime hosts and all their services in Icinga.
 
-    return parser
+    - Optionally force a Puppet run on the Icinga host to pick up new hosts or services
+    - Set Icinga downtime for the given time with a default of 4h if not specified
+
+    Usage example:
+      cookbook sre.hosts.downtime --days 5 'cp1234*'
+      cookbook sre.hosts.downtime --minutes 20 cp1234.eqiad.wmnet
+      cookbook sre.hosts.downtime --minutes 20 'O:cache::upload'
+
+    """
+
+    DEFAULT_DOWNTIME_HOURS = 4
+
+    def argument_parser(self):
+        """As specified by Spicerack API."""
+        parser = argparse.ArgumentParser(description=self.__doc__,
+                                         formatter_class=argparse.RawDescriptionHelpFormatter)
+        parser.add_argument('query', help='Cumin query to match the host(s) to act upon.')
+        parser.add_argument('-r', '--reason', required=True,
+                            help=('The reason for the downtime. The current username and originating host are '
+                                  'automatically added.'))
+        parser.add_argument('-t', '--task-id',
+                            help='An optional task ID to refer in the downtime message (i.e. T12345).')
+        parser.add_argument('-M', '--minutes', type=int, default=0,
+                            help='For how many minutes the downtime should last. [optional, default=0]')
+        parser.add_argument('-H', '--hours', type=int, default=0,
+                            help='For how many hours the downtime should last. [optional, default=0]')
+        parser.add_argument('-D', '--days', type=int, default=0,
+                            help='For how many days the downtime should last. [optional, default=0]')
+        parser.add_argument('--force-puppet', action='store_true',
+                            help='Force a Puppet run on the Icinga host to pick up new hosts or services.')
+
+        return parser
+
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        if not any((args.minutes, args.hours, args.days)):
+            logger.info('No downtime length option specified, using default value of %d hours',
+                        Downtime.DEFAULT_DOWNTIME_HOURS)
+            args.hours = Downtime.DEFAULT_DOWNTIME_HOURS
+
+        return DowntimeRunner(args, self.spicerack)
 
 
-def post_process_args(args):
-    """Do any post-processing of the parsed arguments."""
-    if not any((args.minutes, args.hours, args.days)):
-        logger.info('No downtime length option specified, using default value of %d hours', DEFAULT_DOWNTIME_HOURS)
-        args.hours = DEFAULT_DOWNTIME_HOURS
+class DowntimeRunner(CookbookRunnerBase):
+    """Donwtime cookbook runner class."""
 
+    def __init__(self, args, spicerack):
+        """Initialize the runner."""
+        self.duration = timedelta(days=args.days, hours=args.hours, minutes=args.minutes)
+        self.hosts = spicerack.remote().query(args.query).hosts
+        if not self.hosts:
+            raise RuntimeError('No host found for query "{query}"'.format(query=args.query))
 
-def run(args, spicerack):
-    """Required by Spicerack API."""
-    post_process_args(args)
-    duration = timedelta(days=args.days, hours=args.hours, minutes=args.minutes)
-    remote_hosts = spicerack.remote().query(args.query)
-    icinga = spicerack.icinga()
-    reason = spicerack.admin_reason(args.reason, task_id=args.task_id)
-    phabricator = spicerack.phabricator(PHABRICATOR_BOT_CONFIG_FILE)
+        self.task_id = args.task_id
+        self.icinga = spicerack.icinga()
+        self.reason = spicerack.admin_reason(args.reason, task_id=args.task_id)
 
-    if args.force_puppet:
-        puppet = spicerack.puppet(spicerack.icinga_master_host)
-        logging.info('Forcing a Puppet run on the Icinga server')
-        puppet.run(quiet=True, attempts=30)
+        if args.force_puppet:
+            self.puppet = spicerack.puppet(spicerack.icinga_master_host)
+        else:
+            self.puppet = None
 
-    logging.info('Downtiming %d hosts and all their services for %s: %s', len(remote_hosts), duration, remote_hosts)
-    icinga.downtime_hosts(remote_hosts.hosts, reason, duration=duration)
+        if args.task_id is not None:
+            self.phabricator = spicerack.phabricator(PHABRICATOR_BOT_CONFIG_FILE)
+        else:
+            self.phabricator = None
 
-    if args.task_id is not None:
-        message = ('Icinga downtime for {duration} set by {owner} on {n} host(s) and their services '
-                   'with reason: {reason}\n```\n{hosts}\n```').format(
-            duration=duration, owner=reason.owner, n=len(remote_hosts), reason=args.reason, hosts=remote_hosts)
-        phabricator.task_comment(args.task_id, message)
+        if len(self.hosts) <= 5:
+            hosts_message = str(self.hosts)
+        else:
+            hosts_message = '{num} hosts'.format(num=len(self.hosts))
+
+        self.short_message = ('for {duration} on {hosts_message} with reason: {reason}').format(
+            duration=self.duration, hosts_message=hosts_message, reason=args.reason)
+
+        self.long_message = ('Icinga downtime set by {owner} for {s.duration} {num} host(s) and their services '
+                             'with reason: {reason}\n```\n{s.hosts}\n```').format(
+                                 owner=self.reason.owner, s=self, num=len(self.hosts), reason=args.reason)
+
+    @property
+    def runtime_description(self):
+        """Return a nicely formatted string that represents the downtime action."""
+        return self.short_message
+
+    def run(self):
+        """Required by Spicerack API."""
+        if self.puppet is not None:
+            logging.info('Forcing a Puppet run on the Icinga server')
+            self.puppet.run(quiet=True, attempts=30)
+
+        logging.info('Downtiming %s', self.runtime_description)
+        self.icinga.downtime_hosts(self.hosts, self.reason, duration=self.duration)
+
+        if self.phabricator is not None:
+            self.phabricator.task_comment(self.task_id, self.long_message)
