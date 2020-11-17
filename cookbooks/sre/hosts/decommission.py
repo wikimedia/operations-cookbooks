abcb76427e1f21553915a0fa1533a03e30d67760
Riccardo Coccioli <rcoccioli@wikimedia.org>
2020-11-17 10:21:54 +0100

    sre.hosts.decommission: workaround Netbox cache
    * There is an issue with the internal Netbox cache that makes the call
      to put_host_status() fail with 500 on the API side and with an
      django.db.utils.IntegrityError on the Django side due to a
      psycopg2.errors.ForeignKeyViolation error.
      What happens is that after having deleted the primary IPs the UI shows
      them empty as it should but when instantiating the device object,
      the primary_ip4_id and primary_ip6_id properties still reference the
      old IDs, so Netbox tries to lookup their values and fail because were
      deleted.
    * Workaround the issue explicitely setting them to None while changing
      the status.
    * TODO: find a minimal repro setting, try it on the latest netbox and
      open a bug upstream if confirmed.
    
    Change-Id: Ic6ed3443be8f25f85a56bafbce8abf8a86bfce57

--- cookbooks/sre/hosts/decommission.py
+++ cookbooks/sre/hosts/decommission.py
@@ -213,7 +213,14 @@ def update_netbox(netbox, netbox_data, dry_run):
         else:
             logger.debug('No IPs on interface %s', interface.name)
 
-    netbox.put_host_status(netbox_data['name'], 'Decommissioning')
+    # TODO: this is needed instead of calling put_hosts_status() because Netbox cache doesn't get invalidated
+    #       immediately making the call to fail. A sleep of 10 seconds did not fix the issue either.
+    device = netbox.api.dcim.devices.get(netbox_data['name'])
+    device.primary_ip4_id = None
+    device.primary_ip6_id = None
+    device.status = 'decommissioning'
+    if not dry_run:
+        device.save()
 
 
 def get_grep_patterns(dns, decom_hosts):
