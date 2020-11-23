f77ba5db26fb63148d4b9d92d5233c69671bdea4
Reuven Lazarus <rlazarus@wikimedia.org>
2020-11-23 18:59:50 -0500

    sre.switchdc.mediawiki: Add a step to restart Envoy on jobrunners.
    In the last switchover, we found that jobrunners in DC_FROM were
    continuing to try to edit, reporting errors from the DB master
    (correctly) rejecting edits due to being in read-only mode.
    
    The cause was that changeprop maintains a long-lived connection to the
    jobrunners. When we updated the jobrunners' DNS record to point to
    DC_TO, changeprop didn't notice, and continued using its existing
    connection to jobrunners in DC_FROM.
    
    With this change, we restart Envoy (running as a TLS terminator on the
    server side) on all jobrunners in DC_FROM after the switchover. When
    Envoy shuts down, changeprop closes its long-running connection and
    opens a new one. Because it re-resolves the jobrunner DNS entry in the
    process, it picks up the change and starts talking to DC_TO.
    
    Change-Id: Ibf5160b6b75b7ebfce46b0165e775ce26b9add09

---
+++ cookbooks/sre/switchdc/mediawiki/08-restart-envoy-on-jobrunners.py
@@ -0,0 +1,20 @@
+"""Restart Envoy on jobrunners so changeprop re-resolves the jobrunner service name for its long-running connections."""
+import logging
+
+from cookbooks.sre.switchdc.mediawiki import argument_parser_base, post_process_args
+
+
+__title__ = __doc__
+logger = logging.getLogger(__name__)
+
+
+def argument_parser():
+    """As specified by Spicerack API."""
+    return argument_parser_base(__name__, __title__)
+
+
+def run(args, spicerack):
+    """Required by Spicerack API."""
+    post_process_args(args)
+    logger.info('Restarting Envoy on jobrunners in %s', args.dc_from)
+    spicerack.remote().query('A:mw-jobrunner-{dc}'.format(dc=args.dc_from)).run_sync('systemctl restart envoyproxy')
