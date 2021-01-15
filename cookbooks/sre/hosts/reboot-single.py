fe6e47875a7ab1ba6888d873a7fd483943b1befa
Luca <ltoscano@wikimedia.org>
2021-01-15 08:30:35 +0100

    sre.hosts.reboot-single: move to class API
    Change-Id: I1a0bdbfee30ff815dcaab29310e26cbb0f63a2e6

--- cookbooks/sre/hosts/reboot-single.py
+++ cookbooks/sre/hosts/reboot-single.py
@@ -1,24 +1,12 @@
-r"""Downtime a single host and reboot it
+"""Downtime a single host and reboot it"""
 
-- Set Icinga downtime
-- Reboot
-- Wait for host to come back online
-- Remove the Icinga downtime after the host has been rebooted and the
-  first Puppet run is complete
-
-This is meant for one off servers and doesn't support pooling/depooling
-clustered services (yet).
-
-Usage example:
-    cookbook sre.hosts.reboot-single sretest1001.eqiad.wmnet
-
-"""
 import argparse
 import logging
 import time
 
 from datetime import datetime, timedelta
 
+from spicerack.cookbook import CookbookBase, CookbookRunnerBase
 from spicerack.decorators import retry
 from spicerack.icinga import IcingaError
 
@@ -26,71 +14,98 @@ __title__ = 'Downtime a single host and reboot it'
 logger = logging.getLogger(__name__)
 
 
-def argument_parser():
-    """Parse arguments"""
-    parser = argparse.ArgumentParser(description=__doc__,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('host', help='A single host to be rebooted (specified in Cumin query syntax)')
-    parser.add_argument('--depool', help='Wether to run depool/pool on the server around reboots.',
-                        action='store_true')
-    return parser
+class RebootSingleHost(CookbookBase):
+    """Downtime a single host and reboot it
+
+    - Set Icinga downtime
+    - Reboot
+    - Wait for host to come back online
+    - Remove the Icinga downtime after the host has been rebooted and the
+      first Puppet run is complete
+
+    This is meant for one off servers and doesn't support pooling/depooling
+    clustered services (yet).
+
+    Usage example:
+        cookbook sre.hosts.reboot-single sretest1001.eqiad.wmnet
+
+    """
+
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return RebootSingleHostRunner(args, self.spicerack)
+
+    def argument_parser(self):
+        """Parse arguments"""
+        parser = argparse.ArgumentParser(description=self.__doc__,
+                                         formatter_class=argparse.RawDescriptionHelpFormatter)
+        parser.add_argument('host', help='A single host to be rebooted (specified in Cumin query syntax)')
+        parser.add_argument('--depool', help='Whether to run depool/pool on the server around reboots.',
+                            action='store_true')
+        return parser
 
 
-@retry(tries=20, delay=timedelta(seconds=3), backoff_mode='linear', exceptions=(IcingaError,))
-def wait_for_icinga_optimal(icinga, remote_host):
-    """Waits for an icinga optimal status, else raises an exception."""
-    status = icinga.get_status(remote_host.hosts)
-    if not status.optimal:
-        failed = ["{}:{}".format(k, ','.join(v)) for k, v in status.failed_services.items()]
-        raise IcingaError('Not all services are recovered: {}'.format(' '.join(failed)))
+class RebootSingleHostRunner(CookbookRunnerBase):
+    """Downtime a single host and reboot it runner."""
 
+    def __init__(self, args, spicerack):
+        """Downtime a single host and reboot it"""
+        self.remote_host = spicerack.remote().query(args.host)
 
-def run(args, spicerack):
-    """Reboot the host"""
-    remote_host = spicerack.remote().query(args.host)
+        if len(self.remote_host) == 0:
+            raise RuntimeError('Specified server not found, bailing out')
 
-    if len(remote_host) == 0:
-        logger.error('Specified server not found, bailing out')
-        return 1
+        if len(self.remote_host) != 1:
+            raise RuntimeError('Only a single server can be rebooted')
 
-    if len(remote_host) != 1:
-        logger.error('Only a single server can be rebooted')
-        return 1
+        self.icinga = spicerack.icinga()
+        self.puppet = spicerack.puppet(self.remote_host)
+        self.reason = spicerack.admin_reason('Rebooting host')
+        self.depool = args.depool
 
-    icinga = spicerack.icinga()
-    puppet = spicerack.puppet(remote_host)
-    reason = spicerack.admin_reason('Rebooting host')
+    @retry(tries=20, delay=timedelta(seconds=3), backoff_mode='linear', exceptions=(IcingaError,))
+    def _wait_for_icinga_optimal(self):
+        """Waits for an icinga optimal status, else raises an exception."""
+        status = self.icinga.get_status(self.remote_host.hosts)
+        if not status.optimal:
+            failed = ["{}:{}".format(k, ','.join(v)) for k, v in status.failed_services.items()]
+            raise IcingaError('Not all services are recovered: {}'.format(' '.join(failed)))
 
-    with icinga.hosts_downtimed(remote_host.hosts, reason, duration=timedelta(minutes=20)):
-        if args.depool:
-            remote_host.run_async('depool')
-            logger.info('Waiting a 30 second grace period after depooling')
-            time.sleep(30)
-        reboot_time = datetime.utcnow()
-        remote_host.reboot()
-        remote_host.wait_reboot_since(reboot_time)
-        puppet.wait_since(reboot_time)
+    @property
+    def runtime_description(self):
+        """Return a nicely formatted string that represents the cookbook action."""
+        return 'for host {}'.format(self.remote_host.hosts)
 
-        # First let's try to check if icinga is already in optimal state.
-        # If not, we require a recheck all service, then
-        # wait a grace period before declaring defeat.
-        icinga_ok = icinga.get_status(remote_host.hosts).optimal
-        if not icinga_ok:
-            icinga.recheck_all_services(remote_host.hosts)
-            try:
-                wait_for_icinga_optimal(icinga, remote_host)
-                icinga_ok = True
-            except IcingaError as e:
-                logger.error(str(e))
+    def run(self):
+        """Reboot the host"""
+        with self.icinga.hosts_downtimed(
+                self.remote_host.hosts, self.reason, duration=timedelta(minutes=20)):
+            if self.depool:
+                self.remote_host.run_async('depool')
+                logger.info('Waiting a 30 second grace period after depooling')
+                time.sleep(30)
+            reboot_time = datetime.utcnow()
+            self.remote_host.reboot()
+            self.remote_host.wait_reboot_since(reboot_time)
+            self.puppet.wait_since(reboot_time)
 
-        if args.depool:
-            if icinga_ok:
-                remote_host.run_async('pool')
-            else:
-                logger.warning("NOT repooling the services.")
+            # First let's try to check if icinga is already in optimal state.
+            # If not, we require a recheck all service, then
+            # wait a grace period before declaring defeat.
+            icinga_ok = self.icinga.get_status(self.remote_host.hosts).optimal
+            if not icinga_ok:
+                self.icinga.recheck_all_services(self.remote_host.hosts)
+                try:
+                    self._wait_for_icinga_optimal()
+                    icinga_ok = True
+                except IcingaError:
+                    logger.error(
+                        "The host's status is not optimal according to Icinga, "
+                        "please check it.")
 
-        # Return an error if icinga didn't recover.
-        if not icinga_ok:
-            return 1
-
-    return 0
+            if self.depool:
+                if icinga_ok:
+                    self.remote_host.run_async('pool')
+                else:
+                    logger.warning(
+                        "NOT repooling the services due to the host's Icinga status.")
