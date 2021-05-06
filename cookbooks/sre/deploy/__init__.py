11d0a5ee6a7a64bbb00f0eca7601a2ffe750847c
Riccardo Coccioli <rcoccioli@wikimedia.org>
2021-05-06 18:33:18 +0200

    sre.deploy.python-code: add new cookbook
    * Add a new cookbook that is meant to run the python-deploy-venv script
      to deploy a Python source code that requires a virtualenv on hosts
      where Scap is not yet available.
    
    Change-Id: I2f8ad6ac74bfc5feb0b10d37a9a12bb4dae482c2

---
+++ cookbooks/sre/deploy/__init__.py
@@ -0,0 +1,3 @@
+"""Deployments Cookbooks."""
+__title__ = __doc__
+DEPLOYMENT_CNAME = 'deployment.eqiad.wmnet'  # It resolves to the right one also when switched to codfw
---
+++ cookbooks/sre/deploy/python-code.py
@@ -0,0 +1,86 @@
+"""Deploy a simple Python code software."""
+import argparse
+import logging
+
+from spicerack.cookbook import CookbookBase, CookbookRunnerBase
+from wmflib.interactive import confirm_on_failure
+
+from cookbooks.sre import PHABRICATOR_BOT_CONFIG_FILE
+from cookbooks.sre.deploy import DEPLOYMENT_CNAME
+
+
+logger = logging.getLogger(__name__)
+
+
+class Deploy(CookbookBase):
+    """Deploy a simple Python code software with frozen wheels.
+
+    It mimicks Scap for places where it's not yet available in scenarios like the ones described in:
+    https://phabricator.wikimedia.org/T180023
+    The deploy is done sequentially, one host at a time, pausing and asking the user what to do on error.
+
+    Usage example:
+      cookbook sre.deploy.python-code -r 'some reason' 'someproject' 'A:somealias'
+      cookbook sre.deploy.python-code -r 'some reason' -t T12345 'someproject' 'somehost.example.com'
+
+    """
+
+    def argument_parser(self):
+        """As specified by Spicerack API."""
+        parser = argparse.ArgumentParser(description=self.__doc__,
+                                         formatter_class=argparse.RawDescriptionHelpFormatter)
+        parser.add_argument('project', help='The name of the project on the deployment server.')
+        parser.add_argument('query', help='Cumin query to match the host(s) to act upon.')
+        parser.add_argument('-r', '--reason', required=True,
+                            help=('The reason for the downtime. The current username and originating host are '
+                                  'automatically added.'))
+        parser.add_argument('-t', '--task-id',
+                            help='An optional task ID to refer in the downtime message (i.e. T12345).')
+
+        return parser
+
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return DeployRunner(args, self.spicerack)
+
+
+class DeployRunner(CookbookRunnerBase):
+    """Deploy Python code runner class."""
+
+    def __init__(self, args, spicerack):
+        """Initialize the runner."""
+        remote = spicerack.remote()
+        self.remote_hosts = remote.query(args.query)
+        if not self.remote_hosts.hosts:
+            raise RuntimeError(f'No host found for query "{args.query}"')
+
+        self.project = args.project
+        self.task_id = args.task_id
+        self.deployment_host = remote.query(spicerack.dns().resolve_cname(DEPLOYMENT_CNAME))
+        self.reason = spicerack.admin_reason(args.reason, task_id=args.task_id)
+
+        if args.task_id is not None:
+            self.phabricator = spicerack.phabricator(PHABRICATOR_BOT_CONFIG_FILE)
+        else:
+            self.phabricator = None
+
+        if len(self.remote_hosts) <= 5:
+            hosts_message = str(self.remote_hosts)
+        else:
+            hosts_message = f'{len(self.remote_hosts)} hosts matching {args.query}'
+
+        self.message = (f'{self.project} to {hosts_message} with reason: {self.reason}')
+
+    @property
+    def runtime_description(self):
+        """Return a nicely formatted string that represents the downtime action."""
+        return self.message
+
+    def run(self):
+        """Required by Spicerack API."""
+        self.deployment_host.run_sync(f'/usr/bin/git -C "/srv/deployment/{self.project}/deploy" update-server-info')
+        for remote_host in self.remote_hosts.split(len(self.remote_hosts)):  # Do one host at a time
+            confirm_on_failure(remote_host.run_sync, f'/usr/local/bin/python-deploy-venv {self.project}')
+
+        if self.phabricator is not None:
+            self.phabricator.task_comment(self.task_id, f'Deployed {self.message}')
