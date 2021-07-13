c476a6ebe36dec2e5a9a6263fb009ec800e231dc
Ben Tullis <btullis@wikimedia.org>
2021-07-13 10:37:11 +0100

    Update AQS Roll Restart cookbook to use new style
    A new class API was added to Spicerack recently and this commit
    brings the sre.aqs.roll_restart cookbook into line with the new
    programming style
    
    Bug: T269925
    Change-Id: Ia2999cdab8b8b44b28e1551c8b05cd1c0a08d143

--- cookbooks/sre/aqs/roll-restart.py
+++ cookbooks/sre/aqs/roll-restart.py
@@ -4,55 +4,78 @@ import logging
 
 from datetime import timedelta
 
+from spicerack.cookbook import CookbookBase, CookbookRunnerBase
 from wmflib.interactive import ask_confirmation, ensure_shell_is_durable
 
 from cookbooks import ArgparseFormatter
 
-
-__title__ = 'Roll restart all the nodejs service daemons on the AQS cluster'
 logger = logging.getLogger(__name__)
 
 
-def argument_parser():
-    """As specified by Spicerack API."""
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=ArgparseFormatter)
-    parser.add_argument(
-        'cluster', help='The name of the AQS cluster to work on.', choices=['aqs'])
-    return parser
+class RollRestart(CookbookBase):
+    """Roll restart all the nodejs service daemons on the AQS cluster.
+
+    - Optionally set the name of the AQS cluster to work on.
+
+    Usage example:
+        cookbook.sre.aqs.roll_restart --cluster aqs
+
+    """
+
+    def argument_parser(self):
+        """As specified by Spicerack API."""
+        parser = argparse.ArgumentParser(description=self.__doc__, formatter_class=ArgparseFormatter)
+        parser.add_argument(
+            'cluster', help='The name of the AQS cluster to work on.', choices=['aqs'])
+        return parser
+
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return RollRestartRunner(args, self.spicerack)
 
 
-def run(args, spicerack):
-    """Restart all AQS nodejs service daemons on a given cluster"""
-    ensure_shell_is_durable()
-    remote = spicerack.remote()
-    confctl = spicerack.confctl('node')
-    aqs_canary = remote.query('A:' + args.cluster + '-canary')
-    aqs_workers = remote.query('A:' + args.cluster)
-    icinga_hosts = spicerack.icinga_hosts(aqs_workers.hosts)
-    reason = spicerack.admin_reason('Roll restart of all AQS\'s nodejs daemons.')
+class RollRestartRunner(CookbookRunnerBase):
+    """AQS Roll Restart cookbook runner class"""
 
-    ask_confirmation(
-        'If a config change is being rolled-out, please run puppet on all hosts '
-        'before proceeding.')
+    def __init__(self, args, spicerack):
+        """Initialize the runner."""
+        ensure_shell_is_durable()
+        self.remote = spicerack.remote()
+        self.confctl = spicerack.confctl('node')
+        self.aqs_canary = self.remote.query('A:' + args.cluster + '-canary')
+        self.aqs_workers = self.remote.query('A:' + args.cluster)
+        self.icinga_hosts = spicerack.icinga_hosts(self.aqs_workers.hosts)
+        self.reason = spicerack.admin_reason('Roll restart of all AQS\'s nodejs daemons.')
 
-    with icinga_hosts.downtimed(reason, duration=timedelta(minutes=60)):
-        logger.info("Depool and test on canary: %s", aqs_canary.hosts)
-        aqs_canary.run_sync(
-            'depool',
-            'systemctl restart aqs'
-        )
-        ask_confirmation('Please test aqs on the canary.')
-        logger.info('Pool the canary back.')
-        aqs_canary.run_sync('pool')
+    @property
+    def runtime_description(self):
+        """Return a nicely formatted string that represents the cookbook action."""
+        return 'for AQS {} cluster: {}'.format(self.aqs_workers, self.reason)
 
-        aqs_lbconfig = remote.query_confctl(
-            confctl, cluster=args.cluster,
-            name=r'(?!' + aqs_canary.hosts[0] + ').*')
+    def run(self):
+        """Required by Spicerack API."""
+        ask_confirmation(
+            'If a config change is being rolled-out, please run puppet on all hosts '
+            'before proceeding.')
 
-        logger.info('Restarting remaining daemons (one host at the time).')
-        aqs_lbconfig.run(
-            'systemctl restart aqs', svc_to_depool=['aqs'],
-            batch_size=1, max_failed_batches=2,
-            batch_sleep=30.0)
+        with self.icinga_hosts.downtimed(self.reason, duration=timedelta(minutes=60)):
+            logger.info("Depool and test on canary: %s", self.aqs_canary.hosts)
+            self.aqs_canary.run_sync(
+                'depool',
+                'systemctl restart aqs'
+            )
+            ask_confirmation('Please test aqs on the canary.')
+            logger.info('Pool the canary back.')
+            self.aqs_canary.run_sync('pool')
 
-    logger.info("All AQS service restarts completed!")
+            aqs_lbconfig = self.remote.query_confctl(
+                self.confctl, cluster=self.aqs_workers,
+                name=r'(?!' + self.aqs_canary.hosts[0] + ').*')
+
+            logger.info('Restarting remaining daemons (one host at the time).')
+            aqs_lbconfig.run(
+                'systemctl restart aqs', svc_to_depool=['aqs'],
+                batch_size=1, max_failed_batches=2,
+                batch_sleep=30.0)
+
+        logger.info("All AQS service restarts completed!")
