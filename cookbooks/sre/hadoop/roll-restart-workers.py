cd59859ca611eceea30c2c1e63f2dcc922dc066a
Ben Tullis <btullis@wikimedia.org>
2021-07-14 09:45:35 +0100

    Update sre.hadoop.roll-restart-workers cookbook
    We have recently improved the spicerack API. This commit updates the
    cookbook that is used to restart the Hadoop worker processes so that it
    makes use of these changes to the API.
    
    Bug: T269925
    Change-Id: I3b269da14ca24b2261f21588fb7c80cf71433c1f

--- cookbooks/sre/hadoop/roll-restart-workers.py
+++ cookbooks/sre/hadoop/roll-restart-workers.py
@@ -1,106 +1,130 @@
-"""Restart all Hadoop jvm daemons on worker hosts.
-
-The idea for this cookbook is to:
-- Set Icinga downtime for all nodes (no puppet disable or depool is needed).
-- Roll restart all the Yarn Node Managers in batches of 4/5 hosts.
-- Roll restart all the HDFS Journalnodes, one at the time,
-  with 30s of delay in between.
-- Roll restart all the HDFS Datanodes, a couple of hosts at the time,
-  with 30s of delay in between.
-- Remove Icinga downtime
-"""
+"""Restart all Hadoop jvm daemons on worker hosts."""
 import argparse
 import logging
 
 from datetime import timedelta
 
+from spicerack.cookbook import CookbookBase, CookbookRunnerBase
 from wmflib.interactive import ask_confirmation, ensure_shell_is_durable
 
 from cookbooks import ArgparseFormatter
 from . import HADOOP_CLUSTER_NAMES
 
 
-__title__ = 'Roll restart all the jvm daemons on Hadoop worker nodes'
 logger = logging.getLogger(__name__)
 
 
-def argument_parser():
-    """As specified by Spicerack API."""
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=ArgparseFormatter)
-    parser.add_argument('cluster', help='The name of the Hadoop cluster to work on.',
-                        choices=HADOOP_CLUSTER_NAMES)
-    parser.add_argument('--yarn-nm-sleep-seconds', type=float, default=30.0,
-                        help="Seconds to sleep between each batch of Yarn Nodemanager restarts.")
-    parser.add_argument('--hdfs-dn-sleep-seconds', type=float, default=120.0,
-                        help="Seconds to sleep between each batch of HDFS Datanode restarts.")
-    parser.add_argument('--hdfs-jn-sleep-seconds', type=float, default=120.0,
-                        help="Seconds to sleep between each batch of HDFS Journalnode restarts.")
-    parser.add_argument('--yarn-nm-batch-size', type=int, default=5,
-                        help="Size of each batch of Yarn Nodemanager restarts.")
-    parser.add_argument('--hdfs-dn-batch-size', type=int, default=2,
-                        help="Size of each batch of HDFS Datanode restarts.")
+class RollRestartWorkers(CookbookBase):
+    """Roll restart all the jvm daemons on Hadoop worker nodes.
 
-    return parser
+    - Set Icinga downtime for all nodes (no puppet disable or depool is needed).
+    - Roll restart all the Yarn Node Managers in batches of 5 hosts.
+    - Roll restart all the HDFS Journalnodes, one at a time,
+    with 30s of delay in between.
+    - Roll restart all the HDFS Datanodes, a couple of hosts at a time,
+    with 30s of delay in between.
+    - Remove Icinga downtime.
+    - Batch sizes and delay periods are configurable.
+
+    Usage example:
+      cookbook sre.hadoop.roll-restart-workers analytics
+      cookbook sre.hadoop.roll-restart-workers --yarn-nm-batch-size 2 --hdfs-dn-batch-size 1 test
+      cookbook sre.hadoop.roll-restart-workers --yarn-nm-sleep-seconds 60 --hdfs-dn-sleep-seconds 180 backup
 
 
-def run(args, spicerack):
-    """Restart all Hadoop jvm daemons on a given cluster"""
-    if args.cluster == 'test':
-        cluster_cumin_alias = 'A:hadoop-worker-test'
-        hdfs_jn_cumin_alias = 'A:hadoop-hdfs-journal-test'
-    elif args.cluster == 'analytics':
-        cluster_cumin_alias = 'A:hadoop-worker'
-        hdfs_jn_cumin_alias = 'A:hadoop-hdfs-journal'
-    else:
-        raise RuntimeError("Hadoop cluster {} not supported.".format(args.cluster))
+    """
 
-    ensure_shell_is_durable()
+    def argument_parser(self):
+        """As specified by Spicerack API."""
+        parser = argparse.ArgumentParser(description=self.__doc__, formatter_class=ArgparseFormatter)
+        parser.add_argument('cluster', help='The name of the Hadoop cluster to work on.',
+                            choices=HADOOP_CLUSTER_NAMES)
+        parser.add_argument('--yarn-nm-sleep-seconds', type=float, default=30.0,
+                            help="Seconds to sleep between each batch of Yarn Nodemanager restarts.")
+        parser.add_argument('--hdfs-dn-sleep-seconds', type=float, default=120.0,
+                            help="Seconds to sleep between each batch of HDFS Datanode restarts.")
+        parser.add_argument('--hdfs-jn-sleep-seconds', type=float, default=120.0,
+                            help="Seconds to sleep between each batch of HDFS Journalnode restarts.")
+        parser.add_argument('--yarn-nm-batch-size', type=int, default=5,
+                            help="Size of each batch of Yarn Nodemanager restarts.")
+        parser.add_argument('--hdfs-dn-batch-size', type=int, default=2,
+                            help="Size of each batch of HDFS Datanode restarts.")
 
-    """Required by Spicerack API."""
-    hadoop_workers = spicerack.remote().query(cluster_cumin_alias)
-    hadoop_hdfs_journal_workers = spicerack.remote().query(hdfs_jn_cumin_alias)
-    icinga_hosts = spicerack.icinga_hosts(hadoop_workers.hosts)
-    reason = spicerack.admin_reason('Roll restart of jvm daemons for openjdk upgrade.')
+        return parser
 
-    yarn_nm_batch_size = args.yarn_nm_batch_size
-    yarn_nm_sleep = args.yarn_nm_sleep_seconds
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return RollRestartWorkersRunner(args, self.spicerack)
 
-    # Not configurable on purpose, too risky!
-    hdfs_jn_batch_size = 1
-    hdfs_jn_sleep = args.hdfs_jn_sleep_seconds
 
-    hdfs_dn_batch_size = args.hdfs_dn_batch_size
-    hdfs_dn_sleep = args.hdfs_dn_sleep_seconds
+class RollRestartWorkersRunner(CookbookRunnerBase):
+    """Hadoop Roll Restart Workers cookbook runner class"""
 
-    # Safety checks
-    if hdfs_dn_batch_size > 5:
-        ask_confirmation('The HDFS Datanode batch size is bigger than 5, are you sure?')
-    if hdfs_dn_sleep < 20:
-        ask_confirmation('The HDFS Datanode sleep between each batch is less than 20s, are you sure?')
-    if hdfs_jn_sleep < 20:
-        ask_confirmation('The HDFS Journalnode sleep between each batch is less than 20s, are you sure?')
-    if yarn_nm_batch_size > 10:
-        ask_confirmation('The Yarn Nodemanager batch size is bigger than 10, are you sure?')
-    if yarn_nm_sleep < 20:
-        ask_confirmation('The Yarn Nodemanager sleep between each batch is less than 20s, are you sure?')
+    def __init__(self, args, spicerack):
+        """Initialize the runner"""
+        if args.cluster == 'test':
+            self.cluster_cumin_alias = 'A:hadoop-worker-test'
+            self.hdfs_jn_cumin_alias = 'A:hadoop-hdfs-journal-test'
+        elif args.cluster == 'analytics':
+            self.cluster_cumin_alias = 'A:hadoop-worker'
+            self.hdfs_jn_cumin_alias = 'A:hadoop-hdfs-journal'
+        else:
+            raise RuntimeError("Hadoop cluster {} not supported.".format(args.cluster))
 
-    with icinga_hosts.downtimed(reason, duration=timedelta(minutes=120)):
-        logger.info("Restarting Yarn Nodemanagers with batch size %s and sleep %s..",
-                    yarn_nm_batch_size, yarn_nm_sleep)
-        hadoop_workers.run_sync(
-            'systemctl restart hadoop-yarn-nodemanager',
-            batch_size=yarn_nm_batch_size, batch_sleep=yarn_nm_sleep)
+        ensure_shell_is_durable()
 
-        logger.info("Restarting HDFS Datanodes with batch size %s and sleep %s..",
-                    hdfs_dn_batch_size, hdfs_dn_sleep)
-        hadoop_workers.run_sync(
-            'systemctl restart hadoop-hdfs-datanode',
-            batch_size=hdfs_dn_batch_size, batch_sleep=hdfs_dn_sleep)
+        self.cluster = args.cluster
+        self.hadoop_workers = spicerack.remote().query(self.cluster_cumin_alias)
+        self.hadoop_hdfs_journal_workers = spicerack.remote().query(self.hdfs_jn_cumin_alias)
+        self.icinga_hosts = spicerack.icinga_hosts(self.hadoop_workers.hosts)
+        self.reason = spicerack.admin_reason('Roll restart of jvm daemons for openjdk upgrade.')
 
-        logger.info("Restarting HDFS Journalnodes with batch size %s and sleep %s..",
-                    hdfs_jn_batch_size, hdfs_jn_sleep)
-        hadoop_hdfs_journal_workers.run_sync(
-            'systemctl restart hadoop-hdfs-journalnode',
-            batch_size=hdfs_jn_batch_size, batch_sleep=hdfs_jn_sleep)
+        self.yarn_nm_batch_size = args.yarn_nm_batch_size
+        self.yarn_nm_sleep = args.yarn_nm_sleep_seconds
 
-    logger.info("All jvm restarts completed!")
+        # Not configurable on purpose, too risky!
+        self.hdfs_jn_batch_size = 1
+        self.hdfs_jn_sleep = args.hdfs_jn_sleep_seconds
+
+        self.hdfs_dn_batch_size = args.hdfs_dn_batch_size
+        self.hdfs_dn_sleep = args.hdfs_dn_sleep_seconds
+
+        # Safety checks
+        if self.hdfs_dn_batch_size > 5:
+            ask_confirmation('The HDFS Datanode batch size is bigger than 5, are you sure?')
+        if self.hdfs_dn_sleep < 20:
+            ask_confirmation('The HDFS Datanode sleep between each batch is less than 20s, are you sure?')
+        if self.hdfs_jn_sleep < 20:
+            ask_confirmation('The HDFS Journalnode sleep between each batch is less than 20s, are you sure?')
+        if self.yarn_nm_batch_size > 10:
+            ask_confirmation('The Yarn Nodemanager batch size is bigger than 10, are you sure?')
+        if self.yarn_nm_sleep < 20:
+            ask_confirmation('The Yarn Nodemanager sleep between each batch is less than 20s, are you sure?')
+
+    @property
+    def runtime_description(self):
+        """Return a nicely formatted string that represents the cookbook action."""
+        return 'restart workers for Hadoop {} cluster: {}'.format(self.cluster, self.reason)
+
+    def run(self):
+        """Restart all Hadoop jvm daemons on a given cluster"""
+        with self.icinga_hosts.downtimed(self.reason, duration=timedelta(minutes=120)):
+            logger.info("Restarting Yarn Nodemanagers with batch size %s and sleep %s..",
+                        self.yarn_nm_batch_size, self.yarn_nm_sleep)
+            self.hadoop_workers.run_sync(
+                'systemctl restart hadoop-yarn-nodemanager',
+                batch_size=self.yarn_nm_batch_size, batch_sleep=self.yarn_nm_sleep)
+
+            logger.info("Restarting HDFS Datanodes with batch size %s and sleep %s..",
+                        self.hdfs_dn_batch_size, self.hdfs_dn_sleep)
+            self.hadoop_workers.run_sync(
+                'systemctl restart hadoop-hdfs-datanode',
+                batch_size=self.hdfs_dn_batch_size, batch_sleep=self.hdfs_dn_sleep)
+
+            logger.info("Restarting HDFS Journalnodes with batch size %s and sleep %s..",
+                        self.hdfs_jn_batch_size, self.hdfs_jn_sleep)
+            self.hadoop_hdfs_journal_workers.run_sync(
+                'systemctl restart hadoop-hdfs-journalnode',
+                batch_size=self.hdfs_jn_batch_size, batch_sleep=self.hdfs_jn_sleep)
+
+        logger.info("All jvm restarts completed!")
