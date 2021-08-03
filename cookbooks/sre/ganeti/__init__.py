a0d408613d1d7f17400819a919af2a0a828db27c
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2021-08-03 09:59:24 +0200

    Support test cluster in get_locations()
    Change-Id: I20d02d4366c99222dd06b07b7e042cbb5b9b526c

--- cookbooks/sre/ganeti/__init__.py
+++ cookbooks/sre/ganeti/__init__.py
@@ -10,11 +10,15 @@ def get_locations():
     In the edge DCs the Ganeti servers are in a single row (and the location name is
     identical to the data centre name), but for eqiad/codfw it's a combination of
     DC name and row, e.g. "eqiad_D".
+
+    For added complexity, there's a separate test cluster which may run on the same
     """
     locations = {}
     for cluster, rows in CLUSTERS_AND_ROWS.items():
         dc = cluster.split('.')[2]
-        if len(rows) == 1:
+        if len(rows) == 1 and 'test01' in cluster:
+            locations['{dc}_test'.format(dc=dc)] = (cluster, rows[0], dc)
+        elif len(rows) == 1:
             locations[dc] = (cluster, rows[0], dc)
         else:
             for row in rows:
