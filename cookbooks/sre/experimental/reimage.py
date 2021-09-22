428f87b971edd3b72b005646c55bc268e08834a2
Riccardo Coccioli <rcoccioli@wikimedia.org>
2021-09-22 18:10:10 +0200

    sre.experimental.reimage: fix log messages
    Change-Id: I8429b0f7e9f72e8c7221ae035652b00736baa718

--- cookbooks/sre/experimental/reimage.py
+++ cookbooks/sre/experimental/reimage.py
@@ -151,7 +151,7 @@ class ReimageRunner(CookbookRunnerBase):  # pylint: disable=too-many-instance-at
             self._repool()
 
         self.host_actions.failure('The reimage failed, see the cookbook logs for the details')
-        logger.error('Reimage executed with errors:\n{self.actions}\n')
+        logger.error('Reimage executed with errors:\n%s\n', self.actions)
         if self.phabricator is not None:
             self.phabricator.task_comment(
                 self.args.task_id, f'Cookbook {__name__} executed with errors:\n{self.actions}\n')
@@ -371,7 +371,7 @@ class ReimageRunner(CookbookRunnerBase):  # pylint: disable=too-many-instance-at
         self._repool()
 
         # Comment on the Phabricator task
-        logger.info('Reimage completed:\n{self.actions}\n')
+        logger.info('Reimage completed:\n%s\n', self.actions)
         if self.phabricator is not None:
             self.phabricator.task_comment(self.args.task_id, f'Cookbook {__name__} completed:\n{self.actions}\n')
 
