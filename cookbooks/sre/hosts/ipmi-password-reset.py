c2642360d01bfd137de27be32b8e5f359a01ff80
Riccardo Coccioli <rcoccioli@wikimedia.org>
2021-10-28 13:18:50 +0200

    sre.hosts.ipmi-password-reset: support new hosts
    * Add a --new CLI argument to allow to act on new host that is not yet
      in PuppetDB
    * Convert the use of the deprecated management module to the netbox one
      instead for getting the management FQDN.
    
    Change-Id: I7ccfc05ad841759643d86e05c821a5ac4201ee0a

--- cookbooks/sre/hosts/ipmi-password-reset.py
+++ cookbooks/sre/hosts/ipmi-password-reset.py
@@ -19,7 +19,7 @@ from wmflib.interactive import ensure_shell_is_durable
 
 from spicerack.interactive import get_management_password
 from spicerack.ipmi import IPMI_PASSWORD_MIN_LEN, IPMI_PASSWORD_MAX_LEN, IpmiError, IpmiCheckError
-from spicerack.management import ManagementError
+from spicerack.netbox import NetboxError
 
 
 __title__ = 'Reset the hosts IPMI password'
@@ -37,18 +37,30 @@ def argument_parser():
         '-t', '--task-id',
         help='An optional task ID to update, also used in log messages (i.e. T12345).'
     )
+    parser.add_argument(
+        '--new',
+        action='store_true',
+        help=('For new hardware not yet in PuppetDB, used in conjunction with a query of the form D{FQDN1,FQDN2}.')
+    )
     return parser
 
 
 def run(args, spicerack):
     """Required by Spicerack API."""
     ensure_shell_is_durable()
-    mgmt = spicerack.management()
     remote = spicerack.remote()
-    # start by collecting all physical hosts
-    remote_hosts = remote.query('F:virtual = physical').hosts
-    # get the hosts that intersect with the user query
-    remote_hosts &= remote.query(args.query).hosts
+    if args.new:
+        remote_hosts = remote.query(args.query).hosts
+    else:
+        # start by collecting all physical hosts
+        remote_hosts = remote.query('F:virtual = physical').hosts
+        # get the hosts that intersect with the user query
+        remote_hosts &= remote.query(args.query).hosts
+
+    if not remote_hosts:
+        logger.info('No hosts selected, bailing out.')
+        return 1
+
     reason = spicerack.admin_reason(
         'Updating IPMI password on {} hosts'.format(len(remote_hosts)),
         task_id=args.task_id
@@ -78,13 +90,13 @@ def run(args, spicerack):
         break
 
     spicerack.irc_logger.info(reason)
-    host_status = {'success': NodeSet(), 'fail_dns': NodeSet(), 'fail_ipmi': NodeSet()}
+    host_status = {'success': NodeSet(), 'fail_netbox': NodeSet(), 'fail_ipmi': NodeSet()}
     for host in remote_hosts:
         try:
-            mgmt_host = mgmt.get_fqdn(host)
-        except ManagementError as error:
-            logger.warning('unable to resolve the mgmt address for %s: %s', host, error)
-            host_status['fail_dns'].add(host)
+            mgmt_host = spicerack.netbox_server(host.split('.')[0]).mgmt_fqdn
+        except NetboxError as error:
+            logger.warning('unable to get the mgmt address from Netbox for %s: %s', host, error)
+            host_status['fail_netbox'].add(host)
             continue
 
         ipmi = spicerack.ipmi(mgmt_host)
@@ -100,10 +112,10 @@ def run(args, spicerack):
     The following hosts completed successfully:
         {}
 
-    The following hosts management address failed to resolve:
+    The following hosts were unable to get the management address from Netbox:
         {}
 
     The following hosts had ipmi failures:
         {}
-        '''.format(host_status['success'], host_status['fail_dns'], host_status['fail_ipmi'])
+        '''.format(host_status['success'], host_status['fail_netbox'], host_status['fail_ipmi'])
     logger.info(message)
