5e2bc3525833e80b9047465050df07e93da01c7d
Riccardo Coccioli <rcoccioli@wikimedia.org>
2022-02-15 17:30:25 +0100

    Remove ArgparseFormatter as it's now unused
    * All the cookbooks import the same class from spicerack.cookbook.
    
    Change-Id: I9bba48c520e0433b861d00234d4a44c9bb56ac9a

--- cookbooks/__init__.py
+++ cookbooks/__init__.py
@@ -1,9 +1,2 @@
 """Cookbooks"""
-import argparse
-
-
 __title__ = __doc__
-
-
-class ArgparseFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
-    """Custom argparse formatter for cookbooks."""
