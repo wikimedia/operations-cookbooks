7955df0574ef96aeb8face39367f800ec6fa9c85
David Caro <dcaro@wikimedia.org>
2022-06-14 11:15:50 +0200

    wmcs.ceph: don't use sre upgrade-and-reboot
    It turns out that sre.upgrade-and-reboot uses spicerack's builtin
    alerting/downtiming, so we can't use this, instead copy the logic and
    use our own downtiming (using the cli, instead of the non-public
    api).
    
    Bug: T309786
    Change-Id: I339e567e05ea4136da0f634a02544726f0d90d4c
    Signed-off-by: David Caro <dcaro@wikimedia.org>

--- cookbooks/wmcs/ceph/upgrade_ceph_node.py
+++ cookbooks/wmcs/ceph/upgrade_ceph_node.py
@@ -6,17 +6,14 @@ Usage example:
 
 """
 import argparse
-import importlib
+import datetime
 import logging
 
 from spicerack import Spicerack
 from spicerack.cookbook import ArgparseFormatter, CookbookBase, CookbookRunnerBase
 
-from cookbooks.wmcs import CephClusterController, wrap_with_sudo_icinga
+from cookbooks.wmcs import CephClusterController, CephOSDFlag, downtime_host, uptime_host
 
-# Ugly hack to work around the fact that the module has a non-valid identifier
-# file name
-upgrade_and_reboot = importlib.import_module("cookbooks.sre.hosts.upgrade-and-reboot")
 LOGGER = logging.getLogger(__name__)
 
 
@@ -88,23 +85,38 @@ class UpgradeCephNodeRunner(CookbookRunnerBase):
         if not self.skip_maintenance:
             controller.set_maintenance(force=self.force)
 
-        upgrade_and_reboot.run(
-            args=upgrade_and_reboot.argument_parser().parse_args(
-                [
-                    self.to_upgrade_fqdn,
-                    "--depool-cmd",
-                    "true",
-                    "--repool-cmd",
-                    "true",
-                    "--sleep",
-                    "0",
-                    "--use-sudo",
-                ]
-            ),
-            spicerack=wrap_with_sudo_icinga(my_spicerack=self.spicerack),
+        # Can't use sre upgrade-and-reboot due to it using alertmanager's api to downtime hosts
+        remote_host = self.spicerack.remote().query(self.to_upgrade_fqdn, use_sudo=True)
+        host_name = self.to_upgrade_fqdn.split(".", 1)[0]
+        puppet = self.spicerack.puppet(remote_host)
+        downtime_id = downtime_host(
+            spicerack=self.spicerack,
+            host_name=host_name,
+            comment="Ceph node software upgrade and reboot",
+            duration="20m",
         )
+        puppet_reason = self.spicerack.admin_reason("Software upgrade and reboot")
 
+        with puppet.disabled(puppet_reason):
+            # Upgrade all packages, leave config files untouched, do not prompt
+            upgrade_cmd = (
+                "DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' "
+                "-o Dpkg::Options::='--force-confold' dist-upgrade"
+            )
+            remote_host.run_sync(upgrade_cmd)
+
+            reboot_time = datetime.datetime.utcnow()
+            remote_host.reboot()
+            remote_host.wait_reboot_since(reboot_time)
+
+        puppet.run()
+
+        uptime_host(spicerack=self.spicerack, host_name=host_name, silence_id=downtime_id)
+
+        # Once the node is up, let it rebalance
+        controller.unset_osdmap_flag(CephOSDFlag("norebalance"))
         controller.wait_for_cluster_healthy(consider_maintenance_healthy=True, timeout_seconds=300)
+        controller.set_osdmap_flag(CephOSDFlag("norebalance"))
 
         if not self.skip_maintenance:
             controller.unset_maintenance(force=self.force)
