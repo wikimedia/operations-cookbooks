adc09e622193fc4f9e6306a40bbcc67036748e79
Janis Meybohm <jmeybohm@wikimedia.org>
2022-08-16 17:09:38 +0200

    sre.discovery.service-route: Make the cookbook work
    Due to a couple of bugs the cookbook was not working at all.
    This change should make it work and ensure that only discovery records
    that need a change are worked on.
    
    Bug: T260663
    Change-Id: Id212ef31054a677ab5fc7c6af0fb4a68a630c3f0

--- cookbooks/sre/discovery/service-route.py
+++ cookbooks/sre/discovery/service-route.py
@@ -7,7 +7,7 @@ from spicerack.confctl import ConfctlError
 from spicerack.constants import CORE_DATACENTERS
 from spicerack.cookbook import ArgparseFormatter
 
-from cookbooks.sre.discovery import check_record_for_dc, wipe_recursor_cache, update_ttl
+from cookbooks.sre.discovery import check_record_for_dc, update_ttl
 
 __title__ = __doc__
 logger = logging.getLogger(__name__)
@@ -79,34 +79,39 @@ def check(args, spicerack):
 def pool_or_depool(args, spicerack, depool: bool):
     """Pool/Depool services from given datacenters."""
     pool = not depool  # Just easier to read later on
-    did_change = False
 
     dnsdisc = spicerack.discovery(*args.services)
+    action_services = []
     try:
-        active_dcs = dnsdisc.active_datacenters
+        for service, active_dcs in dnsdisc.active_datacenters.items():
+            if pool and args.datacenter not in active_dcs:
+                # This services needs to be pooled in args.datacenter
+                action_services.append(service)
+            elif depool and args.datacenter in active_dcs:
+                # This service needs to be depooled in args.datacenter
+                action_services.append(service)
     except ConfctlError as e:
         logger.error('dnsdisc %s: %s', args.services, e)
         return 1
 
-    if pool and not all(dc in active_dcs for dc in args.datacenter):
-        old_ttl = update_ttl(dnsdisc, DNS_TTL_MEDIUM)
-        dnsdisc.pool(args.datacenter)
-        did_change = True
-
-    if depool and any(dc in active_dcs for dc in args.datacenter):
-        old_ttl = update_ttl(dnsdisc, DNS_TTL_SHORT)
-        dnsdisc.depool(args.datacenter)
-        did_change = True
-
     # Exit early if no changes where necessary
-    if not did_change:
+    if len(action_services) == 0:
+        logger.info('All services are already in the desired state')
         return 0
+
+    dnsdisc = spicerack.discovery(*action_services)
+    old_ttl = update_ttl(dnsdisc, DNS_TTL_MEDIUM)
+    if pool:
+        dnsdisc.pool(args.datacenter)
+    elif depool:
+        dnsdisc.depool(args.datacenter)
 
     # The actual work is done now. DNS should be propagated in old_ttl seconds from now at the latest.
     records_propagated_at = time.time() + old_ttl
 
     if args.wipe_cache:
-        wipe_recursor_cache(args, spicerack.remote())
+        records = ' '.join([f'{service}.discovery.wmnet' for service in action_services])
+        spicerack.run_cookbook('sre.dns.wipe-cache', [records])
 
     sleep_time = records_propagated_at - time.time()
     if sleep_time > 0:
@@ -115,7 +120,7 @@ def pool_or_depool(args, spicerack, depool: bool):
             time.sleep(sleep_time)
 
     # This just checks auth servers
-    # Fixme: Check the availability of the records on the resolvers as well?
+    # FIXME: Check the availability of the records on the resolvers as well?
     return check(args, spicerack)
 
 
