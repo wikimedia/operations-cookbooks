c98e9bd69fbb933edfc944a45a2f924983afe420
Riccardo Coccioli <rcoccioli@wikimedia.org>
2022-09-22 10:55:53 +0200

    roll-restart-reboot-docker-registry: fix docstring
    * Move the docstring with the example usages to the class docstring so
      that it will be included in the help message automatically.
    
    Change-Id: I8bb5d1d9df92e1f9727803bc5f17ffc0c5c2acfe

--- cookbooks/sre/misc-clusters/roll-restart-reboot-docker-registry.py
+++ cookbooks/sre/misc-clusters/roll-restart-reboot-docker-registry.py
@@ -1,24 +1,22 @@
-"""Cookbook to perform a rolling reboot/restart the Docker registry
-
-Usage example:
-    cookbook sre.misc-clusters.roll-restart-reboot-docker-registry \
-       --reason "Rolling reboot to pick up new kernel" reboot
-
-    cookbook sre.misc-clusters.roll-restart-reboot-docker-registry \
-       --reason "Rolling restart to pick new OpenSSL" restart_daemons
-
-"""
-
+"""Docker registry roll operations cookbook."""
 from cookbooks.sre import SREBatchBase, SRELBBatchRunnerBase
 
 
 class DockerRegistryRestartReboot(SREBatchBase):
-    """Class to roll-restart or -reboot the Docker registry"""
+    """Cookbook to perform a rolling reboot/restart the Docker registry
+
+    Usage example:
+        cookbook sre.misc-clusters.roll-restart-reboot-docker-registry \
+           --reason "Rolling reboot to pick up new kernel" reboot
+
+        cookbook sre.misc-clusters.roll-restart-reboot-docker-registry \
+        --reason "Rolling restart to pick new OpenSSL" restart_daemons
+
+    """
 
     batch_default = 1
     grace_sleep = 2
 
-    # Required
     def get_runner(self, args):
         """As specified by Spicerack API."""
         return DockerRegistryRestartRebootRunner(args, self.spicerack)
