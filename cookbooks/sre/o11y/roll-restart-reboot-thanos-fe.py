7fe35534d8fc422ff9dae960e79c2ade46389730
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-09-27 11:53:24 +0200

    New cookbook to roll-restart/reboot Thanos frontends
    Change-Id: I6fb01797fda75512bd5fe7b89fa31dcbd7c95f68

---
+++ cookbooks/sre/o11y/roll-restart-reboot-thanos-fe.py
@@ -0,0 +1,37 @@
+"""Thanos frontend roll operations cookbook."""
+from cookbooks.sre import SREBatchBase, SRELBBatchRunnerBase
+
+
+class ThanosFrontendRestartReboot(SREBatchBase):
+    """Cookbook to perform a rolling reboot/restart of Thanos frontends
+
+    Usage example:
+        cookbook sre.o11y.roll-restart-reboot-thanos-fe \
+           --reason "Rolling reboot to pick up new kernel" reboot
+
+        cookbook sre.o11y.roll-restart-reboot-thanos-fe \
+           --reason "Rolling restart to pick new OpenSSL" restart_daemons
+
+    """
+
+    batch_default = 1
+    grace_sleep = 2
+
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return ThanosFrontendRestartRebootRunner(args, self.spicerack)
+
+
+class ThanosFrontendRestartRebootRunner(SRELBBatchRunnerBase):
+    """Roll reboot/restart a Thanos frontend cluster"""
+
+    @property
+    def allowed_aliases(self):
+        """Required by SREBatchRunnerBase"""
+        return ['thanos-fe', 'thanos-fe-codfw', 'thanos-fe-eqiad']
+
+    @property
+    def restart_daemons(self):
+        """Return a list of daemons to restart when using the restart action"""
+        return ['swift-proxy', 'thanos-bucket-web', 'thanos-query-frontend'
+                'thanos-query', 'thanos-store']
