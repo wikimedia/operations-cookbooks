1989d8f7e034b0934a121b566c40d48377e0d216
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2022-09-28 14:14:25 +0200

    New cookbook to roll-restart (or roll-reboot) the eventschemas cluster(s)
    Change-Id: I87fd0d1decdec795cdfeea503a5e0688cfa5d318

---
+++ cookbooks/sre/misc-clusters/roll-restart-reboot-eventschemas.py
@@ -0,0 +1,36 @@
+"""Event schemas roll operations cookbook."""
+from cookbooks.sre import SREBatchBase, SRELBBatchRunnerBase
+
+
+class EventSchemasRestartReboot(SREBatchBase):
+    """Cookbook to perform a rolling reboot/restart of EventSchemas
+
+    Usage example:
+        cookbook sre.misc-clusters.roll-restart-reboot-eventschemas \
+           --reason "Rolling reboot to pick up new kernel" reboot
+
+        cookbook sre.misc-clusters.roll-restart-reboot-eventschemas \
+        --reason "Rolling restart to pick new OpenSSL" restart_daemons
+
+    """
+
+    batch_default = 1
+    grace_sleep = 2
+
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return EventSchemasRestartRebootRunner(args, self.spicerack)
+
+
+class EventSchemasRestartRebootRunner(SRELBBatchRunnerBase):
+    """Roll reboot/restart an EventSchemas cluster"""
+
+    @property
+    def allowed_aliases(self):
+        """Required by SRELatchRunnerBase"""
+        return ['schema', 'schema-eqiad', 'schema-codfw']
+
+    @property
+    def restart_daemons(self):
+        """Return a list of daemons to restart when using the restart action"""
+        return ['nginx']
