6585c68d4b97ff1ce66726778dbe729c9b30a2cf
Brian King <bking@wikimedia.org>
2022-12-07 16:39:31 -0600

    wdqs data-reload.py: fix usage comment
    Fix usage example comment to include required argument "--reload-data".
    
    Change-Id: I9d0f9f5204dd8ef28eea6f1240ebba0db819a7c8

--- cookbooks/sre/wdqs/data-reload.py
+++ cookbooks/sre/wdqs/data-reload.py
@@ -1,7 +1,8 @@
 """WDQS data reload
 
 Usage example:
-    cookbook sre.wdqs.data-reload wdqs1010.eqiad.wmnet --reason 'fix issues' --task-id T12345
+    cookbook sre.wdqs.data-reload --reload-data wikidata --reason "bring new hosts into rotation" \
+    --task-id T301167 wdqs1004.eqiad.wmnet
 
 """
 
