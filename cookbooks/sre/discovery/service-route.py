f564a329d3a7e0a28857887ae2b7ac66078dde82
Luca Toscano <ltoscano@wikimedia.org>
2022-12-19 17:57:03 +0100

    sre.discovery.service-route: refactor to base/runner classes
    With the new format we'll get a better SAL logging and
    code organization (more inline with the majority of
    the cookbooks).
    
    Bug: T277677
    Change-Id: I647a5f29543be3feb62a724f812979156acab556

--- cookbooks/sre/discovery/service-route.py
+++ cookbooks/sre/discovery/service-route.py
@@ -1,15 +1,14 @@
 """DNS Discovery Operations"""
-import argparse
 import logging
 import time
 
+from spicerack import Spicerack
+from spicerack.cookbook import CookbookBase, CookbookRunnerBase
 from spicerack.confctl import ConfctlError
-from spicerack.cookbook import ArgparseFormatter
 from wmflib.constants import CORE_DATACENTERS
 
 from cookbooks.sre.discovery import check_record_for_dc, update_ttl
 
-__title__ = __doc__
 logger = logging.getLogger(__name__)
 
 # Fixme: Move to spicerack.constants
@@ -18,119 +17,166 @@ DNS_TTL_MEDIUM = 300
 DNS_TTL_SHORT = 10
 
 
-def argument_parser():
-    """Parse the command line arguments for all the sre.discovery cookbooks."""
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=ArgparseFormatter)
-    actions = parser.add_subparsers(dest='action', help='The action to perform')
-    action_check = actions.add_parser('check')
-    action_check.add_argument('services', nargs='+', help='The services to operate on')
-    action_pool = actions.add_parser('pool')
-    action_depool = actions.add_parser('depool')
+class DiscoveryServiceRoute(CookbookBase):
+    """Pool/Depool/Check services via DNS Discovery operations.
 
-    for a in (action_pool, action_depool):
-        a.add_argument('datacenter', choices=CORE_DATACENTERS, help='Name of the datacenter. One of: %(choices)s.')
-        a.add_argument('services', nargs='+', help='The services to operate on')
-        a.add_argument('--wipe-cache', action='store_true', help='Wipe the cache on DNS recursors.')
+    This cookbook automates DNS Discovery operations like pool and depool of
+    specific services.
 
-    return parser
+    Examples:
+    - Check the state of the test-svc service:
+      cookbook.sre.k8s.discovery.service-route check test-svc
+
+    - Depool a list of services from codfw:
+      cookbook.sre.k8s.discovery.service-route depool codfw test-svc test-svc2
+
+    - Pool a list of services from codfw:
+      cookbook.sre.k8s.discovery.service-route pool codfw test-svc test-svc2
+
+    - Depool a service from codfw and wipe the DNS recursors' cache:
+      cookbook.sre.k8s.discovery.service-route pool codfw test-svc test-svc2 --wipe-cache
+
+      Please note: this cookbook does not handle active/active vs active/passive
+      distinctions, it will just execute a pool/depool action as the operator
+      says. It currently does not prevent you from doing unwanted actions like
+      depooling the only active DC of an active/passive svc.
+
+    """
+
+    def argument_parser(self):
+        """Parse the command line arguments for all the sre.discovery cookbooks."""
+        parser = super().argument_parser()
+        parser.add_argument('--reason', required=False, help='Admin reason')
+        actions = parser.add_subparsers(dest='action', help='The action to perform')
+        action_check = actions.add_parser('check')
+        action_check.add_argument('services', nargs='+', help='The services to operate on')
+        action_pool = actions.add_parser('pool')
+        action_depool = actions.add_parser('depool')
+
+        for a in (action_pool, action_depool):
+            a.add_argument('datacenter', choices=CORE_DATACENTERS, help='Name of the datacenter. One of: %(choices)s.')
+            a.add_argument('services', nargs='+', help='The services to operate on')
+            a.add_argument('--wipe-cache', action='store_true', help='Wipe the cache on DNS recursors.')
+
+        return parser
+
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return DiscoveryServiceRouteRunner(args, self.spicerack)
 
 
-def check(args, spicerack):
-    """Check the current state of the service in conftool and on authoritative DNS servers."""
-    dnsdisc = spicerack.discovery(*args.services)
-    try:
-        active_dcs = dnsdisc.active_datacenters
-    except ConfctlError as e:
-        logger.error('dnsdisc %s: %s', args.services, e)
-        return 1
+class DiscoveryServiceRouteRunner(CookbookRunnerBase):
+    """Pool/Depool/Check services via DNS Discovery operations runner class."""
 
-    print('Expected routes:')
-    for svc in args.services:
-        svc_active_dcs = active_dcs.get(svc, [])
-        route = ','.join(sorted(svc_active_dcs))
-        print('{service}: {route}'.format(service=svc, route=route))
-        for dc in CORE_DATACENTERS:
-            dc_to = dc
-            if dc not in svc_active_dcs:
-                # If DC is not active, the discovery record should point to the other DC
-                # WARNING: This assumes we only have wo datacenters.
-                dc_to = svc_active_dcs[0]
+    def __init__(self, args, spicerack: Spicerack):
+        """Initialize the runner."""
+        self.args = args
+        self.spicerack = spicerack
+        dnsdisc = self.spicerack.discovery(*self.args.services)
 
-            # Not all discovery records have a <svc>.<dc>.wmnet record.
-            # For instance, appservers-{rw,ro} will need to resolve appserver.svc.<dc>.wmnet.
-            # WARNING: This is a hard coded assumption that may not cover all cases correctly.
-            svc_to = svc
-            for postfix in ('-rw', '-ro', '-async', '-php'):
-                if svc_to.endswith(postfix):
-                    svc_to = svc_to[:-len(postfix)]
+        try:
+            self.active_dcs = dnsdisc.active_datacenters
+        except ConfctlError as e:
+            logger.error('dnsdisc %s: %s', self.args.services, e)
+            raise RuntimeError(
+                "An error has occurrend while the cookbook was running its init steps"
+            ) from e
 
-            expected_name_fmt = '{service}.svc.{dc_to}.wmnet'
-            if svc_to != svc:
-                logger.info('Stripped prefix from expected target service name: %s -> %s',
-                            expected_name_fmt.format(service=svc, dc_to=dc_to),
-                            expected_name_fmt.format(service=svc_to, dc_to=dc_to))
+        self.action_services = []
+        self.depool = self.args.action == 'depool'
+        self.pool = self.args.action == 'pool'
+        try:
+            for service, self.active_dcs in dnsdisc.active_datacenters.items():
+                if self.pool and self.args.datacenter not in self.active_dcs:
+                    # This services needs to be pooled in args.datacenter
+                    self.action_services.append(service)
+                elif self.depool and self.args.datacenter in self.active_dcs:
+                    # This service needs to be depooled in args.datacenter
+                    self.action_services.append(service)
+        except ConfctlError as e:
+            logger.error('dnsdisc %s: %s', self.args.services, e)
+            raise RuntimeError(
+                "An error has occurrend while the cookbook was running its init steps"
+            ) from e
 
-            # Check if authdns reflects the conftool/etcd setting
-            check_record_for_dc(spicerack.dry_run, dnsdisc, dc, svc,
+        self.dnsdisc = spicerack.discovery(*self.action_services)
+
+    @property
+    def runtime_description(self):
+        """Return a nicely formatted string that represents the cookbook action."""
+        if len(self.args.services) == 1:
+            services_msg = self.args.services[0]
+        else:
+            services_msg = f'{len(self.args.services)} services'
+
+        reason = self.args.reason if self.args.reason else "maintenance"
+        return f"{self.args.action} {services_msg} in {self.args.datacenter}: {reason}"
+
+    def check(self):
+        """Check the current state of the service in conftool and on authoritative DNS servers."""
+        print('Expected routes:')
+        for svc in self.args.services:
+            svc_active_dcs = self.active_dcs.get(svc, [])
+            route = ','.join(sorted(svc_active_dcs))
+            print('{service}: {route}'.format(service=svc, route=route))
+            for dc in CORE_DATACENTERS:
+                dc_to = dc
+                if dc not in svc_active_dcs:
+                    # If DC is not active, the discovery record should point to the other DC
+                    # WARNING: This assumes we only have two datacenters.
+                    dc_to = svc_active_dcs[0]
+
+                # Not all discovery records have a <svc>.<dc>.wmnet record.
+                # For instance, appservers-{rw,ro} will need to resolve appserver.svc.<dc>.wmnet.
+                # WARNING: This is a hard coded assumption that may not cover all cases correctly.
+                svc_to = svc
+                for postfix in ('-rw', '-ro', '-async', '-php'):
+                    if svc_to.endswith(postfix):
+                        svc_to = svc_to[:-len(postfix)]
+
+                expected_name_fmt = '{service}.svc.{dc_to}.wmnet'
+                if svc_to != svc:
+                    logger.info('Stripped prefix from expected target service name: %s -> %s',
+                                expected_name_fmt.format(service=svc, dc_to=dc_to),
                                 expected_name_fmt.format(service=svc_to, dc_to=dc_to))
-    return 0
 
-
-def pool_or_depool(args, spicerack, depool: bool):
-    """Pool/Depool services from given datacenters."""
-    pool = not depool  # Just easier to read later on
-
-    dnsdisc = spicerack.discovery(*args.services)
-    action_services = []
-    try:
-        for service, active_dcs in dnsdisc.active_datacenters.items():
-            if pool and args.datacenter not in active_dcs:
-                # This services needs to be pooled in args.datacenter
-                action_services.append(service)
-            elif depool and args.datacenter in active_dcs:
-                # This service needs to be depooled in args.datacenter
-                action_services.append(service)
-    except ConfctlError as e:
-        logger.error('dnsdisc %s: %s', args.services, e)
-        return 1
-
-    # Exit early if no changes where necessary
-    if len(action_services) == 0:
-        logger.info('All services are already in the desired state')
+                # Check if authdns reflects the conftool/etcd setting
+                check_record_for_dc(self.spicerack.dry_run, self.dnsdisc, dc, svc,
+                                    expected_name_fmt.format(service=svc_to, dc_to=dc_to))
         return 0
 
-    dnsdisc = spicerack.discovery(*action_services)
-    old_ttl = update_ttl(dnsdisc, DNS_TTL_MEDIUM)
-    if pool:
-        dnsdisc.pool(args.datacenter)
-    elif depool:
-        dnsdisc.depool(args.datacenter)
+    def pool_or_depool(self):
+        """Pool/Depool services from given datacenters."""
+        old_ttl = update_ttl(self.dnsdisc, DNS_TTL_MEDIUM)
+        if self.pool:
+            self.dnsdisc.pool(self.args.datacenter)
+        elif self.depool:
+            self.dnsdisc.depool(self.args.datacenter)
 
-    # The actual work is done now. DNS should be propagated in old_ttl seconds from now at the latest.
-    records_propagated_at = time.time() + old_ttl
+        # The actual work is done now. DNS should be propagated in old_ttl seconds from now at the latest.
+        records_propagated_at = time.time() + old_ttl
 
-    if args.wipe_cache:
-        records = ' '.join([f'{service}.discovery.wmnet' for service in action_services])
-        spicerack.run_cookbook('sre.dns.wipe-cache', [records])
+        if self.args.wipe_cache:
+            records = ' '.join([f'{service}.discovery.wmnet' for service in self.action_services])
+            self.spicerack.run_cookbook('sre.dns.wipe-cache', [records])
 
-    sleep_time = records_propagated_at - time.time()
-    if sleep_time > 0:
-        logging.info('Waiting %.2f seconds for DNS changes to propagate', sleep_time)
-        if not spicerack.dry_run:
-            time.sleep(sleep_time)
+        sleep_time = records_propagated_at - time.time()
+        if sleep_time > 0:
+            logging.info('Waiting %.2f seconds for DNS changes to propagate', sleep_time)
+            if not self.spicerack.dry_run:
+                time.sleep(sleep_time)
 
-    # This just checks auth servers
-    # FIXME: Check the availability of the records on the resolvers as well?
-    return check(args, spicerack)
+        # This just checks auth servers
+        # FIXME: Check the availability of the records on the resolvers as well?
+        return self.check()
 
-
-def run(args, spicerack):
-    """Required by Spicerack API."""
-    if args.action == 'check':
-        return check(args, spicerack)
-    if args.action == 'pool':
-        return pool_or_depool(args, spicerack, depool=False)
-    if args.action == 'depool':
-        return pool_or_depool(args, spicerack, depool=True)
-
-    return 0
+    def run(self):
+        """Required by Spicerack API."""
+        # Exit early if no changes where necessary
+        if len(self.action_services) == 0:
+            logger.info('All services are already in the desired state')
+        else:
+            if self.args.action == 'check':
+                self.check()
+            else:
+                self.pool_or_depool()
