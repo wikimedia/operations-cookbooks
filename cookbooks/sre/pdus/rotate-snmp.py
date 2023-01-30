eb73fe9af65132075b4cc62eda47c24ec7ef26bc
John Bond <jbond@wikimedia.org>
2023-01-30 20:31:23 +0100

    rotate-snmp: convert to cookbook classes and use secrets for passwords
    Change-Id: Ibde1b261c01b6ed2fc74ca952835e682f9c68f7e

--- cookbooks/sre/pdus/rotate-snmp.py
+++ cookbooks/sre/pdus/rotate-snmp.py
@@ -1,25 +1,14 @@
-"""Update Sentry PDUs SNMP communities.
-
-- Optionally checks if the default user is still configured.
-- Default user is 'root'
-- If host 'all' is passed, will iterate over all PDUs
-- So try --dry-run first 😉
-
-Usage example:
-    cookbook sre.pdus.rotate-snmp --username MrFoo 'ps1-b5-eqiad.mgmt.eqiad.wmnet'
-    cookbook sre.pdus.rotate-snmp --no-ro all
-    cookbook sre.pdus.rotate-snmp --no-rw all --check_default
-"""
+"""Cookbook to rotate snmp strings"""
 # pylint overrides are for https://bugs.python.org/issue31844 but on 3.10 it's fixed, so adding the useless suppression
 # pylint: disable=useless-suppression
-from base64 import b64encode
 from datetime import datetime
 from html.parser import HTMLParser
 from logging import getLogger
-from os import urandom
+from secrets import token_urlsafe
 from time import sleep
 
 from requests import Session
+from spicerack.cookbook import CookbookBase, CookbookRunnerBase
 from wmflib.interactive import ensure_shell_is_durable, get_secret
 
 from cookbooks.sre import pdus
@@ -101,133 +90,142 @@ class PDUParserV4(PDUParser):  # pylint: disable=abstract-method
         })
 
 
-def random_string(string_length=16):
-    """Return a random string of a specific length
+class ChangeSNMP(CookbookBase):
+    """Update Sentry PDUs SNMP communities.
 
-    Arguments:
-        string_length (int): The length of the random string to generate
+    - Optionally checks if the default user is still configured.
+    - Default user is 'root'
+    - If host 'all' is passed, will iterate over all PDUs
+    - So try --dry-run first 😉
 
-    Returns:
-        str: a random string
-
+    Usage example:
+        cookbook sre.pdus.rotate-snmp --username MrFoo 'ps1-b5-eqiad.mgmt.eqiad.wmnet'
+        cookbook sre.pdus.rotate-snmp --no-ro all
+        cookbook sre.pdus.rotate-snmp --no-rw all --check_default
     """
-    # gut feeling that _- will be safer then += in theory either is fine
-    altchars = b'_-'
-    # TODO: use secrets once we no longer need to support 3.5
-    return b64encode(urandom(string_length), altchars)[:string_length]
+
+    def argument_parser(self):
+        """As specified by Spicerack API."""
+        parser = pdus.argument_parser_base()
+        parser.add_argument('--force', action='store_true',
+                            help='Force an update even if the current values seem correct')
+        parser.add_argument('--reset-rw', action='store_true',
+                            help='Reset the RW community to a random string')
+        return parser
+
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return ChangeSNMPRunner(args, self.spicerack)
 
 
-def change_snmp(pdu, version, session, snmp_ro, snmp_rw=None, force=False):
-    """Change the snmp_string
+class ChangeSNMPRunner(CookbookRunnerBase):
+    """Runner to change PDU SNMP strings."""
 
-    Arguments:
-        pdu (str): the pdu
-        version (int): the sentry version number
-        session (requests.Session): A configured request session
-        snmp_ro (str): The new SNMP RO string
-        snmp_rw (str): The new SNMP RW string
+    def __init__(self, args, spicerack):
+        """Initiate snmpe changer."""
+        ensure_shell_is_durable()
 
-    Returns:
-        bool: Indicate if a string was updated
-    Raises:
-        SnmpResetError
+        password = get_secret('Enter login password')
+        self.snmp_ro = get_secret('New SNMP RO String', confirm=True)
+        self.snmp_rw = token_urlsafe(16) if args.reset_rw else None
 
-    """
-    parser = {
-        3: PDUParserV3,
-        4: PDUParserV4,
-    }.get(version)()
+        self.force = args.force
+        self.reset_rw = args.force
+        self.check_default = args.check_default
 
-    if parser is None:
-        raise SnmpResetError("Unknown Version Sentry 👎")
-    if not snmp_ro and not snmp_rw:
-        raise SnmpResetError("you must provide one of snmp_ro or snmp_rw")
+        self.session = Session()
+        self.session.verify = False
+        self.session.auth = (args.username, password)
+        self.spicerack = spicerack
 
-    snmp_form = 'https://{}/Forms/snmp_1'.format(pdu)
+        self._pdus = pdus.get_pdu_ips(spicerack.netbox(), args.query)
 
-    try:
-        # first fetch the form to get the current values
-        logger.debug('%s: Fetch current values', pdu)
-        response = pdus.get(session, snmp_form)
-        parser.feed(response.content.decode())
-    except pdus.RequestError as err:
-        raise SnmpResetError from err
-    form = parser.form.copy()
-    # update the form paramters with new values
-    if form['GetCom'] != snmp_ro:
-        logger.info('%s: Updating SNMP RO', pdu)
-        form['GetCom'] = snmp_ro
-    if snmp_rw:
-        logger.info('%s: Updating SNMP RW', pdu)
-        form['SetCom'] = snmp_rw
-    # post new values
-    if form == parser.form:
-        uptime = pdus.get_uptime(pdu, session)
-        logger.info('%s: SNMP communities already match (version: %d, uptime: %s)',
-                    pdu, version, uptime)
-        if not force:
-            return False
-        logger.info('%s: Force update', pdu)
-    try:
-        logger.debug('Posting: %s -> %s', form, snmp_form)
-        response = pdus.post(session, snmp_form, form)
-        parser.feed(response.content.decode())
-    except pdus.RequestError as err:
-        raise SnmpResetError from err
-    # Check the new values applied
-    if parser.form['GetCom'] != snmp_ro:
-        raise SnmpResetError('{}: failed to update snmp_ro'.format(pdu))
+    def change_snmp(self, pdu: str, version: str) -> bool:
+        """Change the snmp_string.
 
-    logger.info('%s: SNMP RO: updated', pdu)
-    if snmp_rw and parser.form['SetCom'] != snmp_rw:
-        raise SnmpResetError('{}: failed to update snmp_rw'.format(pdu))
-    if snmp_rw:
-        logger.info('%s: SNMP RW: updated', pdu)
-    return True
+        Arguments:
+            pdu (str): the pdu
+            version (str): The pdu version number
 
+        Returns:
+            bool: Indicate if a string was updated
+        Raises:
+            SnmpResetError
 
-def argument_parser():
-    """As specified by Spicerack API."""
-    parser = pdus.argument_parser_base()
-    parser.add_argument('--force', action='store_true',
-                        help='Reset the RW community to a random string')
-    parser.add_argument('--reset-rw', action='store_true',
-                        help='Reset the RW community to a random string')
-    return parser
+        """
+        version = pdus.get_version(pdu, self.session)
+        parser = {
+            3: PDUParserV3,
+            4: PDUParserV4,
+        }.get(version)()
 
+        if parser is None:
+            raise SnmpResetError("Unknown Version Sentry 👎")
 
-def run(args, spicerack):
-    """Required by Spicerack API."""
-    ensure_shell_is_durable()
-    return_code = 0
-    session = Session()
-    session.verify = False
-    password = get_secret('Enter login password')
-    snmp_ro = get_secret('New SNMP RO String', confirm=True)
+        snmp_form = 'https://{}/Forms/snmp_1'.format(pdu)
 
-    session.auth = (args.username, password)
-
-    _pdus = pdus.get_pdu_ips(spicerack.netbox(), args.query)
-
-    for pdu in _pdus:
-        snmp_rw = random_string() if args.reset_rw else None
         try:
-            if not spicerack.dry_run:
-                version = pdus.get_version(pdu, session)
-                if change_snmp(pdu, version, session, snmp_ro, snmp_rw, args.force):
-                    reboot_time = datetime.utcnow()
-                    pdus.reboot(pdu, version, session)
-                    # Reboots from experience take at least 60 seconds
-                    logger.info('%s: sleep while reboot', pdu)
-                    sleep(60)
-                    pdus.wait_reboot_since(pdu, reboot_time, session)
-            else:
-                logger.info('%s: Dry run, not trying.', pdu)
-            if args.check_default:
-                if pdus.check_default(pdu, session):
-                    # TODO: delete default user
-                    pass
-        except (pdus.VersionError, SnmpResetError, pdus.RebootError) as error:
-            logger.error(error)
-            return_code = 1
-    return return_code
+            # first fetch the form to get the current values
+            logger.debug('%s: Fetch current values', pdu)
+            response = pdus.get(self.session, snmp_form)
+            parser.feed(response.content.decode())
+        except pdus.RequestError as err:
+            raise SnmpResetError from err
+        form = parser.form.copy()
+        # update the form paramters with new values
+        if form['GetCom'] != self.snmp_ro:
+            logger.info('%s: Updating SNMP RO', pdu)
+            form['GetCom'] = self.snmp_ro
+        if self.snmp_rw:
+            logger.info('%s: Updating SNMP RW', pdu)
+            form['SetCom'] = self.snmp_rw
+        # post new values
+        if form == parser.form:
+            uptime = pdus.get_uptime(pdu, self.session)
+            logger.info('%s: SNMP communities already match (version: %d, uptime: %s)',
+                        pdu, version, uptime)
+            if not self.force:
+                return False
+            logger.info('%s: Force update', pdu)
+        try:
+            logger.debug('Posting: %s -> %s', form, snmp_form)
+            response = pdus.post(self.session, snmp_form, form)
+            parser.feed(response.content.decode())
+        except pdus.RequestError as err:
+            raise SnmpResetError from err
+        # Check the new values applied
+        if parser.form['GetCom'] != self.snmp_ro:
+            raise SnmpResetError('{}: failed to update snmp_ro'.format(pdu))
+
+        logger.info('%s: SNMP RO: updated', pdu)
+        if self.snmp_rw and parser.form['SetCom'] != self.snmp_rw:
+            raise SnmpResetError('{}: failed to update snmp_rw'.format(pdu))
+        if self.snmp_rw:
+            logger.info('%s: SNMP RW: updated', pdu)
+        return True
+
+    def run(self):
+        """Required by Spicerack API."""
+        return_code = 0
+
+        for pdu in self._pdus:
+            version = pdus.get_version(pdu, self.session)
+            try:
+                if not self.spicerack.dry_run:
+                    if self.change_snmp(pdu, version):
+                        reboot_time = datetime.utcnow()
+                        pdus.reboot(pdu, version, self.session)
+                        # Reboots from experience take at least 60 seconds
+                        logger.info('%s: sleep while reboot', pdu)
+                        sleep(60)
+                        pdus.wait_reboot_since(pdu, reboot_time, self.session)
+                else:
+                    logger.info('%s: Dry run, not trying.', pdu)
+                if self.check_default:
+                    if pdus.check_default(pdu, self.session):
+                        # TODO: delete default user
+                        pass
+            except (pdus.VersionError, SnmpResetError, pdus.RebootError) as error:
+                logger.error(error)
+                return_code = 1
+        return return_code
