cd00d1b2f4d77177d986735715fba745d287a66e
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2023-02-02 13:41:48 +0100

    Add sre.discovery.datacenter-route
    This cookbook allows to depool/repool all services included in
    the service catalog in a single datacenter.
    
    It will by default only handle the active/active services, as follows:
    * If the service would have no more datacenters where it's pooled, ask
      the user to chose a datacenter to move to, or skip it
    * Pool the service in any datacenter if needed
    * Depool the service in the datacenter if needed
    * Check that every authdns server has picked up the configuration
    * Wipe the recursor cache for this record
    
    For non-mediawiki active/passive services, if passed the --all flag, the
    workflow is slightly different:
    * Based on the options, the user might be asked to pick a datacenter to
      move to or to skip the service
    * the new datacenter is pooled
    * the original one is depooled
    * Check that every authdns server has picked up the configuration
    * Clean up stale confd error templates from authdns servers
    * Wipe the recursor cache for this record
    
    Change-Id: If078ff2a4249953b04384e801549ed23ef053088

---
+++ cookbooks/sre/discovery/datacenter-route.py
@@ -0,0 +1,360 @@
+"""Divert all traffic from a datacenter."""
+import argparse
+import logging
+
+from dataclasses import dataclass
+from typing import Dict, List, Set
+
+from spicerack import Spicerack
+from spicerack.cookbook import CookbookBase, CookbookRunnerBase
+from spicerack.decorators import retry
+from spicerack.dnsdisc import DiscoveryCheckError, DiscoveryError
+from spicerack.remote import RemoteHosts
+from spicerack.service import ServiceDiscoveryRecord, ServiceIPs
+from wmflib.constants import CORE_DATACENTERS
+from wmflib.interactive import ask_input, ask_confirmation, InputError
+
+from cookbooks.sre.discovery import resolve_with_client_ip, DC_IP_MAP
+from cookbooks.sre.switchdc.mediawiki import MEDIAWIKI_SERVICES
+
+
+logger = logging.getLogger(__name__)
+# Avoid conftool logs to flood INFO/DEBUG
+logging.getLogger("conftool").setLevel(logging.WARNING)
+
+# This is used in DiscoveryDcRouteRunner._get_all_services, but might be of use for
+# other cookbooks too, so make it a module constant.
+EXCLUDED_SERVICES = {
+    "blubberoid": "blubberoid needs to follow swift replica for the docker registry",
+    "docker-registry": "swift replica goes codfw => eqiad and needs manual switching",
+    "releases": "not a 'service', strictly speaking, thus excluded",
+    "puppetdb-api": "not a 'service', strictly speaking, thus excluded",
+    "toolhub": "T288685: needs to match m5 database cluster replication",
+}
+
+
+@dataclass(frozen=True)
+class DiscoveryRecord:
+    """Encapsulates the two objects from servicecatalog we need."""
+
+    record: ServiceDiscoveryRecord
+    ips: ServiceIPs
+
+    def depool(self, datacenter: str):
+        """Depools the record from the given datacenter."""
+        self.record.instance.depool(datacenter)
+
+    def pool(self, datacenter: str):
+        """Pools the record in the given datacenter."""
+        self.record.instance.pool(datacenter)
+
+    def clear_cache(self, recursors: RemoteHosts):
+        """Clears the DNS resolver caches for this record."""
+        recursors.run_sync(f"sudo rec_control wipe-cache {self.fqdn}")
+
+    def clean_discovery_templates(self, authdns: RemoteHosts):
+        """Removes spurious dns discovery errors when switching A/P services."""
+        if self.active_active:
+            logger.debug("NOT clearing confd templates for %s as it's an active/active service.", self.name)
+            return
+        authdns.run_sync(f"rm -fv /var/run/confd-template/.discovery-{self.name}.state*.err")
+
+    @retry(backoff_mode="constant", exceptions=(DiscoveryCheckError, DiscoveryError), tries=15)
+    def check_records(self):
+        """Check the DNS records.
+
+        For every datacenter the service is present in, we check that:
+        * If the datacenter is pooled, resolving the name from a client in that datacenter
+          returns the local IP of the service
+        * If it's depooled, resolving the name from a client in that datacenter returns a
+          non-local ip for the service.
+
+        The most important function of this check is to ensure the etcd change has been
+        propagated before we cleare the dns recursor caches.
+
+        Raises: DiscoveryCheckError on failure
+        """
+        # for each service that is depooled, check that the ip returned by the
+        # authoritative resolver is not the one in the same DC.
+        # Opposite for pooled ones.
+        current_state = self.state
+        for datacenter in self.ips.sites:
+            expected_ip = str(self.ips.get(datacenter))
+
+            for dns_answer in resolve_with_client_ip(self.record.instance, DC_IP_MAP[datacenter], self.name):
+                actual_ip = dns_answer[0].address
+                if datacenter in current_state and actual_ip != expected_ip:
+                    raise DiscoveryCheckError(
+                        f"Error checking auth dns for {self.fqdn} in {datacenter}: "
+                        f"resolved to {actual_ip}, expected: {expected_ip}"
+                    )
+                if datacenter not in current_state and actual_ip == expected_ip:
+                    raise DiscoveryCheckError(
+                        f"Error checking auth dns for {self.fqdn} in {datacenter}: "
+                        f"resolved to {expected_ip}, a different IP was expected."
+                    )
+
+    @property
+    def fqdn(self) -> str:
+        """The fqdn of the record"""
+        return f"{self.name}.discovery.wmnet"
+
+    @property
+    def active_active(self) -> bool:
+        """AA or AP"""
+        return self.record.active_active
+
+    @property
+    def name(self) -> str:
+        """The dnsdisc name"""
+        return self.record.dnsdisc
+
+    @property
+    def state(self) -> Set[str]:
+        """The state of the dnsdisc object"""
+        return set(self.record.instance.active_datacenters[self.name])
+
+
+class DiscoveryDcRoute(CookbookBase):
+    """Pool/Depool a datacenter from internal traffic.
+
+    This cookbook automates DNS Discovery operations like pool and depool of
+    an entire core datacenter.
+
+    Examples:
+    - Depool all a/a discovery records in codfw:
+      cookbook.sre.discovery.datacenter-route depool codfw
+
+    - Pool all a/a discovery records in eqiad
+      cookbook.sre.discovery.datacenter-route [--reason REASON] pool eqiad
+
+    - Depool ALL discovery records from codfw:
+      cookbook.sre.discovery.datacenter-route depool codfw --all
+
+    When called without the --all switch, this cookbook will change the pooled
+    state of all active-active to the desired state. It will prevent you from depooling
+    a service that's only active in this datacenter, asking you if you prefer to skip
+    it or move it to another datacenter. After every migration it will wipe the resolver caches to make
+    the move more aggressive as this cookbook can be used in emergency situations.
+
+    When called with the --all switch, this cookbook will try to also move all active/passive services
+    with the notable exception of mediawiki-related services.
+
+    When migrating an active-passive service, the cookbook will first set both datacenters to pooled, which
+    will not trigger a dns change, then depool the desired datacenter, and only then wipe the resolver caches.
+
+    """
+
+    def argument_parser(self):
+        """Parse the command line arguments for this cookbook."""
+        parser = super().argument_parser()
+        parser.add_argument("--reason", required=False, help="Admin reason", default="maintenance")
+        actions = parser.add_subparsers(dest="action", help="The action to perform")
+
+        for act in ["pool", "depool"]:
+            action = actions.add_parser(act)
+            action.add_argument(
+                "datacenter", choices=CORE_DATACENTERS, help="Name of the datacenter. One of: %(choices)s."
+            )
+            action.add_argument(
+                "--all", action="store_true", help="Depool also the active/passive services (minus MediaWiki)"
+            )
+
+        return parser
+
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return DiscoveryDcRouteRunner(args, self.spicerack)
+
+
+class DiscoveryDcRouteRunner(CookbookRunnerBase):
+    """Pool/Depool/Check services via DNS Discovery operations runner class."""
+
+    def __init__(self, args: argparse.Namespace, spicerack: Spicerack):
+        """Set up a runner for the desired action."""
+        self.spicerack = spicerack
+        self.datacenter: str = args.datacenter
+        self.do_all: bool = args.all
+        self.action: str = args.action
+        self.admin_reason: str = args.reason
+        self.catalog = self.spicerack.service_catalog()
+        self.discovery_records = self._get_all_services()
+        # Stores the initial state of all services we've acted upon.
+        # Used for rollbacks.
+        self.initial_state: Dict[str, Set[str]] = {}
+        self._recursors: RemoteHosts = spicerack.remote().query("A:dns-rec")
+        self._authdns: RemoteHosts = spicerack.remote().query("A:dns-auth")
+
+    @property
+    def runtime_description(self) -> str:
+        """Used to log the action performed to SAL"""
+        if self.do_all:
+            services_msg = "all services"
+        else:
+            services_msg = "all active/active services"
+
+        log_msg = f"{self.action} {services_msg} in {self.datacenter}: {self.admin_reason}"
+        return log_msg
+
+    def run(self):
+        """Execute pooling/depooling."""
+        pool = self.action == "pool"
+        # For each A/A discovery record, check if the service is pooled in more than just the datacenter we're depooling
+        # from.
+        # If not, ask the user to choose between skipping it and moving it
+        for record in self.discovery_records["active_active"]:
+            # Store the current state
+            current_state = record.state
+            self.initial_state[record.name] = current_state
+            is_pooled = self.datacenter in current_state
+            # now determine what is the desired state
+            desired_state = current_state.copy()
+            if pool and not is_pooled:
+                desired_state.add(self.datacenter)
+            if not pool and is_pooled:
+                desired_state.remove(self.datacenter)
+            self._handle_active_active(record, current_state, desired_state)
+
+        # For each A/P discovery record, first ensure we're pooling another core datacenter, then depool the current
+        # one. We'll ask confirmation for each of them.
+        for record in self.discovery_records["active_passive"]:
+            # store the current state
+            current_state = record.state
+            self.initial_state[record.name] = current_state
+            desired_state: Set[str] = current_state.copy()
+
+            if pool:
+                # If we're pooling the current datacenter, we want it to be the only datacenter pooled in the end.
+                desired_state = set(self.datacenter)
+            else:
+                # If depooling, we just remove the current datacenter from the desired state.
+                # If desired_state ends up empty, _skip_or_move will be called.
+                desired_state.remove(self.datacenter)
+
+            self._handle_active_passive(record, current_state, desired_state)
+
+    def rollback(self):
+        """Roll back everything we've done."""
+        ask_confirmation("Do you wish to rollback to the state before the cookbook ran?")
+
+        for record in self.discovery_records["active_passive"]:
+            self._handle_active_passive(record, record.state, self.initial_state[record.name])
+
+        for record in self.discovery_records["active_active"]:
+            self._handle_active_active(record, record.state, self.initial_state[record.name])
+
+    def _handle_active_active(self, record: DiscoveryRecord, current_state: Set[str], desired_state: Set[str]):
+        if desired_state == current_state:
+            logger.info("Service %s is already in the desired state", record.name)
+            return
+        # safety check: if the desired state would have the service depooled everywhere, refuse to do anything and go
+        # away, unless the user provides a choice.
+        if len(desired_state) == 0:
+            logger.warning("The current operation would leave service %s completely depooled.", record.name)
+            self._skip_or_move(record, self.datacenter)
+            return
+
+        # Now pool all services that are currently not pooled
+        for datacenter in desired_state - current_state:
+            record.pool(datacenter)
+
+        # And depool all services that are currently pooled and shouldn't be
+        for site in current_state - desired_state:
+            record.depool(site)
+        record.check_records()
+        record.clear_cache(self._recursors)
+
+    def _handle_active_passive(self, record: DiscoveryRecord, current_state: Set[str], desired_state: Set[str]):
+        if desired_state == current_state:
+            logger.info("Service %s is already in the desired state", record.name)
+            return
+
+        # Check current_state size
+        pooled_now = len(current_state)
+        pooled_after = len(desired_state)
+        # safety check: if the service is pooled in multiple datacenters, stop and request a manual solution
+        if pooled_now > 1:
+            ask_confirmation(
+                f"The discovery record {record.name} is A/P but pooled in multiple datacenters. "
+                "Please resolve manually using conftool and confirm resolution. The cookbook will "
+                "NOT act on the service."
+            )
+            return
+
+        if pooled_after > 1:
+            logger.error("We cannot pool %s in multiple datacenters. Ignoring the request.", record.name)
+            return
+
+        if pooled_now == 0:
+            # The service is unpooled. We just assign a random dc_from here.
+            dc_from = "none"
+        else:
+            dc_from = list(current_state).pop()
+
+        if pooled_after == 0:
+            dc_to = None
+        else:
+            dc_to = list(desired_state).pop()
+
+        self._skip_or_move(record, dc_from, dc_to)
+
+    def _get_all_services(self) -> Dict[str, List[DiscoveryRecord]]:
+        all_services: Dict[str, List[DiscoveryRecord]] = {"active_active": [], "active_passive": []}
+        # We exclude:
+        # - mediawiki read/write endpoints
+        # - services that need special handling for switchover
+        # - all active/passive services unless explicitly asked
+        for service in self.catalog:
+            if service.discovery is None:
+                logger.debug("Skipping %s, as it doesn't have a discovery record", service.name)
+                continue
+            if service.name in EXCLUDED_SERVICES:
+                logger.info("Skipping excluded service %s: %s", service.name, EXCLUDED_SERVICES[service.name])
+                continue
+            for record in service.discovery:
+                complete_record = DiscoveryRecord(record=record, ips=service.ip)
+                if complete_record.name in MEDIAWIKI_SERVICES:
+                    logger.info("Skipping %s, (use the sre.switchdc.mediawiki cookbook instead)", complete_record.name)
+                    continue
+                # Do not add active/passive services if do_all is not selected.
+                if complete_record.active_active:
+                    all_services["active_active"].append(complete_record)
+                elif self.do_all:
+                    all_services["active_passive"].append(complete_record)
+        return all_services
+
+    def _skip_or_move(self, record: DiscoveryRecord, dc_from: str, dc_to=None):
+        # For services pooled only in the datacenter we're depooling from, skip them or move them based on user input.
+        available_dcs = [dc for dc in record.ips.sites if dc != dc_from]
+        selected = None
+        if dc_to is not None:
+            dest = dc_to
+            selected = dc_to
+        else:
+            dest = "another datacenter"
+            if len(available_dcs) == 1:
+                selected = available_dcs.pop()
+                dest = selected
+
+        try:
+            action = ask_input(
+                f"{record.fqdn} is only pooled in {dc_from}: skip or move to {dest}?",
+                ["move", "skip"],
+            )
+            if action == "skip":
+                return
+
+            if selected is None:
+                selected = ask_input("Please pick a datacenter to move to", available_dcs)
+        except InputError:
+            logger.error("Invalid responses, NOT acting on record %s", record.fqdn)
+            return
+
+        record.pool(selected)
+        # If we're pooling a previously completely depooled service, we don't need
+        # to depool anything.
+        if dc_from in CORE_DATACENTERS:
+            record.depool(dc_from)
+        record.check_records()
+        record.clean_discovery_templates(self._authdns)
+        record.clear_cache(self._recursors)
