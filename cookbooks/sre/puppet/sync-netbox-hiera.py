682d3f896ee2944cad7a756c60c2790a7a50b003
John Bond <jbond@wikimedia.org>
2023-02-09 17:28:56 +0100

    sre.puppet.sync-netbox-hiera: Use netbox GraphQL endpoint to fetch data
    Update the current cookbook to directly fetch the netbox data using the
    GraphQL endpoint.  This allows us to keep all code in one place and not
    split between the netbox-extra and this repo.
    
    However so far i dont see any real speed improvement:
      $ time sudo cookbook -vvvv  -c /home/jbond/cookbook.yaml --dry-run \
          sre.puppet.sync-netbox-hiera 'testing GraphQL'
      real    0m59.435s
      user    0m2.491s
      sys     0m0.382s
    
      $ time sudo cookbook --dry-run sre.puppet.sync-netbox-hiera \
          'testing GraphQL'
       real    1m0.915s
       user    0m2.381s
       sys     0m0.367s
    
    I have tested this with dry-run mode and the resulting diff is empty
    
    Change-Id: Icd3212529ad8e56a7b29db3e948d920040f24814

--- cookbooks/sre/puppet/sync-netbox-hiera.py
+++ cookbooks/sre/puppet/sync-netbox-hiera.py
@@ -1,6 +1,7 @@
 """Update and deploy the hiera data generated from Netbox data."""
 from argparse import Namespace
 from pathlib import Path
+from typing import Optional, Union
 
 import yaml
 
@@ -11,6 +12,59 @@ from wmflib.interactive import confirm_on_failure
 from spicerack import Spicerack
 from spicerack.cookbook import CookbookBase, CookbookRunnerBase
 from spicerack.reposync import RepoSyncNoChangeError
+
+
+DEVICE_LIST_GQL = """
+query ($role: [String], $status: [String]) {
+    device_list(role: $role, status: $status) {
+        name
+        status
+        site { slug }
+        tenant { name }
+        rack {
+            name
+            location {
+                slug
+            }
+        }
+    }
+}
+"""
+VM_LIST_GQL = """
+query ($status: [String]) {
+    virtual_machine_list(status: $status) {
+        name
+        status
+        tenant { name }
+        cluster {
+            name
+            group { name}
+            site { slug }
+        }
+    }
+}
+"""
+MGMT_LIST_GQL = """
+query {
+    interface_list(
+        mgmt_only: true,
+    ) {
+        name
+        ip_addresses {
+            dns_name
+        }
+        device {
+            status
+            site { slug }
+            tenant { name }
+            rack {
+                name
+                location { slug }
+            }
+        }
+    }
+}
+"""
 
 
 class NetboxHiera(CookbookBase):
@@ -66,17 +120,19 @@ class NetboxHieraRunner(CookbookRunnerBase):
             spicerack (Spicerack): An initiated spicerack object
 
         """
-        config = load_yaml_config(spicerack.config_dir / "netbox" / "config.yaml")
         if args.check and not spicerack.dry_run:
             # Force dry-run mode
             raise RuntimeError("check mode must also be run in --dry-run mode!")
+
+        config = load_yaml_config(spicerack.config_dir / "netbox" / "config.yaml")
+
         self.args = args
         self.reposync = spicerack.reposync("netbox-hiera")
         self.puppetmasters = spicerack.remote().query("A:puppetmaster")
         self.reason = spicerack.admin_reason(args.message, task_id=args.task_id)
-        self.api_url = f"{config['api_url'].rstrip('/')}:8443/hiera_export.HieraExport"
-        self.session = spicerack.requests_session(__name__, timeout=300)
-        self.session.headers.update(
+        self._api_url = f"{config['api_url']}graphql/"
+        self._session = spicerack.requests_session(__name__, timeout=60)
+        self._session.headers.update(
             {"Authorization": f"Token {config['api_token_ro']}"}
         )
 
@@ -85,20 +141,126 @@ class NetboxHieraRunner(CookbookRunnerBase):
         """Required by API"""
         return f"generate netbox hiera data: {self.reason.quoted()}"
 
-    def _get_netbox_data(self) -> dict:
-        """Fetch netbox data.
+    def _gql_execute(self, query: str, variables: Optional[dict] = None) -> dict:
+        """Parse the query into a gql query, execute and return the results
 
-        Returns:
-            dict: dictionary of netbox objects
+        Arguments:
+            query: a string representing the gql query
+            variables: A list of variables to send
+
+        Results:
+            dict: the results
 
         """
+        data: dict[str, Union[str, dict]] = {"query": query}
+        if variables is not None:
+            data["variables"] = variables
         try:
-            response = self.session.get(self.api_url, json={})
+            response = self._session.post(self._api_url, json=data)
             response.raise_for_status()
+            return response.json()['data']
         except RequestException as error:
-            raise RuntimeError(f"failed to fetch netbox data: {error}") from error
+            raise RuntimeError(
+                f"failed to fetch netbox data: {error}\n{response.text}"
+            ) from error
+        except KeyError as error:
+            raise RuntimeError(f"No data found in GraphQL response: {error}") from error
 
-        return response.json()
+    def _devices(self, status: list[str], roles: list[str]) -> dict:
+        """Return the devices data.
+
+        Arguments:
+            roles: the netbox devices roles to filer on
+            status: the netbox status to filter on
+
+        Returns:
+            dict: the management host data
+
+        """
+        results = {}
+        variables = {"role": roles, "status": status}
+        hosts = self._gql_execute(DEVICE_LIST_GQL, variables)['device_list']
+        for host in hosts:
+            # TODO: i think we should be able to filter this stuff out via the
+            # GraphQL query directly,  ut i cant work out how to say is null
+            if host['tenant'] is not None:
+                continue
+
+            data = {
+                'status': host['status'].lower(),
+                'location': {'site': host['site']['slug']},
+            }
+            if host['rack'] is None:
+                continue
+            data['location']['rack'] = host['rack']['name']
+            data['location']['row'] = host['rack']['location']['slug']
+            results[host['name']] = data
+
+        return results
+
+    def _virtual_hosts(self, status: list[str]) -> dict:
+        """Return the Virtual machine data.
+
+        Arguments:
+            roles: the netbox devices roles to filer on
+
+        Returns:
+            dict: the management host data
+
+        """
+        results = {}
+        variables = {"status": status}
+        hosts = self._gql_execute(VM_LIST_GQL, variables)['virtual_machine_list']
+        for host in hosts:
+            # TODO: i think we should be able to filter this stuff out via the
+            # GraphQL query directly
+            if host['status'] not in ['ACTIVE', 'FAILED']:
+                continue
+            if host['tenant'] is not None:
+                continue
+            data = {
+                'status': host['status'].lower(),
+                'location': {
+                    'site': host['cluster']['site']['slug'],
+                    'ganeti_group': host['cluster']['name'],
+                    'ganeti_cluster': host['cluster']['group']['name'],
+                },
+            }
+            results[host['name']] = data
+
+        return results
+
+    def _mgmt_hosts(self) -> dict:
+        """Return the mgmt_host data
+
+        Returns:
+            dict: the management host data
+
+        """
+        results = {}
+        hosts = self._gql_execute(MGMT_LIST_GQL)['interface_list']
+        for host in hosts:
+            # TODO: i think we should be able to filter this stuff out via the
+            # GraphQL query directly
+            if not host['ip_addresses']:
+                continue
+
+            device = host['device']
+            if device['tenant'] is not None:
+                continue
+            if device['status'] in ['OFFLINE', 'PLANNED', 'DECOMMISSIONING']:
+                continue
+
+            data = {
+                'row': device['rack']['location']['slug'],
+                'rack': device['rack']['name'],
+                'site': device['site']['slug'],
+            }
+
+            address = host['ip_addresses'][0]['dns_name']
+            results[address] = data
+
+        return results
 
     def _write_hiera_files(self, out_dir: Path) -> None:
         """Write out all the hiera files.
@@ -107,10 +269,13 @@ class NetboxHieraRunner(CookbookRunnerBase):
             out_dir (Path): The directory to write the data
 
         """
-        data = self._get_netbox_data()
+        valid_status = ['active', 'failed']
+        hosts = self._virtual_hosts(valid_status) | self._devices(
+            valid_status, ['server']
+        )
         hosts_dir = out_dir / "hosts"
         hosts_dir.mkdir()
-        for host, host_data in data["hosts"].items():
+        for host, host_data in hosts.items():
             host_path = hosts_dir / f"{host}.yaml"
             hiera_data = {f"{self.host_prefix}::{k}": v for k, v in host_data.items()}
             with host_path.open("w") as host_fh:
@@ -118,7 +283,7 @@ class NetboxHieraRunner(CookbookRunnerBase):
 
         common_path = out_dir / "common.yaml"
         with common_path.open("w") as common_fh:
-            common_data = {f"{self.hiera_prefix}::data::{k}": v for k, v in data["common"].items()}
+            common_data = {f"{self.hiera_prefix}::data::mgmt": self._mgmt_hosts()}
             yaml.safe_dump(common_data, common_fh, default_flow_style=False)
 
     def update_puppetmasters(self, hexsha: str) -> None:
