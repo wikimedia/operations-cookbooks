3bd98633e347b60bf89ab43ea99b9d14751914f8
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-02-09 14:09:17 +0100

    cookbooks.sre.elasticsearch.restart-nginx: New cookbook
    To facililate restarts to e.g. pick up new library dependencies/config changes
    etc.
    
    Change-Id: I2dc3fd0dceebb72244b35d0eed335ae2ee4b4498

---
+++ cookbooks/sre/elasticsearch/restart-nginx.py
@@ -0,0 +1,37 @@
+"""Roll-restart nginx on Elastic clusters"""
+
+from cookbooks.sre import SREBatchBase, SREBatchRunnerBase
+
+
+class ElasticRestartNginx(SREBatchBase):
+    """cookbook to perform rolling restart of nginx on Elastic
+
+    Usage example:
+        cookbook sre.elasticsearch.restart-nginx --alias relforge \
+           --reason "Rolling restart to pick up OpenSSL update" restart
+
+    """
+
+    batch_default = 1
+    valid_actions = ('restart_daemons',)
+    grace_sleep = 2
+
+    # We must implement this abstract method
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return ElasticRestartNginxRunner(args, self.spicerack)
+
+
+class ElasticRestartNginxRunner(SREBatchRunnerBase):
+    """Roll restart Nginx on an Elastic cluster"""
+
+    @property
+    def allowed_aliases(self):
+        """Required by SREBatchRunnerBase"""
+        return ['cloudelastic', 'elastic', 'elastic-canary', 'elastic-codfw'
+                'elastic-eqiad', 'relforge']
+
+    @property
+    def restart_daemons(self):
+        """Required by RebootRunnerBase"""
+        return ['nginx']
