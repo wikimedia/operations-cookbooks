77eecf0ffadce2357c5b37e4ed39e998c4e7d13b
Riccardo Coccioli <rcoccioli@wikimedia.org>
2023-02-27 10:25:19 +0100

    OS_VERSIONS: add bookworm to the allowed versions
    Bug: T330495
    Change-Id: I439c346de47676268cb8b1e7bf4b1c3be0dbe51b

--- cookbooks/sre/hosts/__init__.py
+++ cookbooks/sre/hosts/__init__.py
@@ -1,4 +1,4 @@
 """Generic hosts Cookbooks"""
 __title__ = __doc__
 # Supported Debian OS versions
-OS_VERSIONS = ('stretch', 'buster', 'bullseye')
+OS_VERSIONS = ('stretch', 'buster', 'bullseye', 'bookworm')
