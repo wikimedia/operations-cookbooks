71641a0e9a6408b483d9f6d5d06255f5d26b0031
Riccardo Coccioli <rcoccioli@wikimedia.org>
2023-03-01 11:50:00 +0100

    sre.loadbalancer.restart-pybal: simplify call
    * Use the spicerack's provided requests_session() instead of importing
      and calling directly the wmflib's one.
    
    Change-Id: I5ed8be471df7fbb0d8759ff9eb2ee93269d9a72c

--- cookbooks/sre/loadbalancer/restart-pybal.py
+++ cookbooks/sre/loadbalancer/restart-pybal.py
@@ -12,7 +12,6 @@ from spicerack.decorators import retry
 from spicerack.exceptions import SpicerackCheckError
 from spicerack.remote import RemoteHosts
 from wmflib.constants import ALL_DATACENTERS, CORE_DATACENTERS
-from wmflib.requests import http_session
 
 from cookbooks.sre import SREBatchBase, SREBatchRunnerBase
 
@@ -61,7 +60,7 @@ class LBRestartRunner(SREBatchRunnerBase):
     def __init__(self, args: argparse.Namespace, spicerack: Spicerack) -> None:
         """Initializes the parent class, also adds an http session"""
         super().__init__(args, spicerack)
-        self._http = http_session("PybalRestart", timeout=2.0, tries=2, backoff=2.0)
+        self._http = spicerack.requests_session(__name__, timeout=2.0, tries=2, backoff=2.0)
 
     @property
     def allowed_aliases(self) -> List:
