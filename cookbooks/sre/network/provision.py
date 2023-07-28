d583e79263d48dab0678fdc39bf2957f344f76ae
Cathal Mooney <cmooney@wikimedia.org>
2023-07-28 18:34:18 +0100

    Increase the number of retries for ZTP provision cookbook
    Increase the number of retries to allow for longer time before device
    is reachable when we pass JunOS image as part of ZTP process.
    
    Bug: T336485
    Change-Id: I1d3b31612018d541fb04e210e55e9defcb5827f7

--- cookbooks/sre/network/provision.py
+++ cookbooks/sre/network/provision.py
@@ -142,7 +142,7 @@ class ProvisionRunner(CookbookRunnerBase):
         # Refresh the netbox device
         self.netbox_device = self._get_netbox_device()
 
-    @retry(tries=20, backoff_mode='linear', failure_message='Device still not reachable, keep polling')
+    @retry(tries=60, backoff_mode='linear', failure_message='Device still not reachable, keep polling')
     def _poll_device(self, remote_device):
         """Poll the device via SSH until its reachable with the Homer's key."""
         command = 'show system uptime local'
