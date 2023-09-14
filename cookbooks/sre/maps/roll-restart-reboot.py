fa22783672d7a849130f64329021f54a4ea93855
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2023-09-14 12:51:09 +0200

    Extend the maps restart cookbook to also handle reboots
    We have a legacy cookbook to handle maps reboots, but since the Cassandra
    shutdown/startup is handled in the systemd units these days, we can simply
    extend the cookbook to also cover reboots.
    
    Bug: T317855
    Change-Id: I757d348dc649de56f4219f3685ad2ab39334a3e2

--- cookbooks/sre/maps/roll-restart-reboot.py
+++ cookbooks/sre/maps/roll-restart-reboot.py
@@ -1,26 +1,27 @@
-"""Maps rolling restart cookbook."""
+"""Maps rolling restart/reboot cookbook."""
 from cookbooks.sre import SREBatchBase, SRELBBatchRunnerBase
 
 
-class MapsRestart(SREBatchBase):
+class MapsRestartReboot(SREBatchBase):
     """Cookbook to perform a rolling restart of maps services
 
     Usage example:
-        cookbook sre.maps.roll-restart \
-        --reason "Rolling restart to pick new OpenSSL" restart_daemons
+        cookbook sre.maps.roll-restart-reboot \
+          --reason "Rolling restart to pick new OpenSSL" restart_daemons
 
+        cookbook sre.maps.roll-restart-reboot \
+          --reason "Rolling reboot to pick up new kernel" reboot
     """
 
     batch_default = 1
-    grace_sleep = 2
-    valid_actions = ('restart_daemons',)
+    grace_sleep = 30
 
     def get_runner(self, args):
         """As specified by Spicerack API."""
-        return MapsRestartRunner(args, self.spicerack)
+        return MapsRestartRebootRunner(args, self.spicerack)
 
 
-class MapsRestartRunner(SRELBBatchRunnerBase):
+class MapsRestartRebootRunner(SRELBBatchRunnerBase):
     """Roll restart an maps cluster"""
 
     @property
@@ -31,4 +32,4 @@ class MapsRestartRunner(SRELBBatchRunnerBase):
     @property
     def restart_daemons(self):
         """Return a list of daemons to restart when using the restart action"""
-        return ['tileratorui', 'kartotherian', 'nginx', 'postgresql']
+        return ['kartotherian', 'nginx', 'postgresql']
