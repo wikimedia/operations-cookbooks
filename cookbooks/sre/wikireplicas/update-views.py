00b71b6fb6200a221cfe71d8ef3216d700273c35
Taavi Väänänen <hi@taavi.wtf>
2023-11-20 14:32:27 +0200

    wikireplicas: update-views: try to do changes live
    Most changes can be done without depooling the hosts, and we're about to
    change how depooling is done.
    
    Change-Id: Ida044fe191a7d799bfa2cb6e5c85f3c743a64a2c

--- cookbooks/sre/wikireplicas/update-views.py
+++ cookbooks/sre/wikireplicas/update-views.py
@@ -2,8 +2,7 @@
 import logging
 
 from spicerack.cookbook import CookbookBase, CookbookRunnerBase
-from spicerack.decorators import retry
-from spicerack.remote import RemoteExecutionError, RemoteHosts
+from spicerack.remote import RemoteExecutionError
 from wmflib.interactive import ask_confirmation, ensure_shell_is_durable
 
 from cookbooks.sre import PHABRICATOR_BOT_CONFIG_FILE
@@ -98,20 +97,6 @@ class UpdateWikireplicaViewsRunner(CookbookRunnerBase):
 
         ask_confirmation("Ready to proceed?")
 
-    def _determine_dbproxy_host_to_update(self, category) -> RemoteHosts:
-        """Find the dbproxy host for the given category (one of analytics/web).
-
-        Returns a RemoteHosts object which can be sent further commands.
-        """
-        query = f"C:profile::mariadb::proxy::multiinstance_replicas%replica_type = {category}"
-
-        dbproxy_hosts = self.remote.query(query)
-
-        if len(dbproxy_hosts) != 1:
-            raise RuntimeError(f"Expected 1 dbproxy host, got {dbproxy_hosts.hosts}")
-
-        return dbproxy_hosts
-
     def _determine_clouddb_hostname_to_update(self, section, category) -> str:
         query = f"P{{O:wmcs::db::wikireplicas::{category}_multiinstance}} and A:db-section-{section}"
 
@@ -121,69 +106,6 @@ class UpdateWikireplicaViewsRunner(CookbookRunnerBase):
             raise RuntimeError(f"Expected 1 clouddb host, got {clouddb_hosts}")
 
         return clouddb_hosts[0]
-
-    def _depool_host(self, dbproxy_host, clouddb_hostname, section):
-        dbproxy_host.run_sync(
-            f'echo "set server {clouddb_hostname} state drain" | socat /run/haproxy/haproxy.sock stdio'
-        )
-
-        grep_command = f"grep -qvz '^mariadb-{section},{clouddb_hostname}'"
-        desired_state = "depooled"
-
-        self._confirm_haproxy_state(
-            dbproxy_host, clouddb_hostname, grep_command, desired_state
-        )
-
-    def _confirm_haproxy_state(
-        self, dbproxy_host, clouddb_hostname, grep_command, desired_state
-    ):
-        try:
-            self._retry_confirm_haproxy_state(dbproxy_host, grep_command)
-            self.actions[dbproxy_host].success(
-                f"Confirmed {clouddb_hostname} is {desired_state} from {dbproxy_host}"
-            )
-        except RemoteExecutionError:
-            self.actions[dbproxy_host].failure(
-                f"**Could not confirm host is {desired_state}**"
-            )
-            raise
-
-    @staticmethod
-    @retry(tries=5, backoff_mode="constant", exceptions=(RemoteExecutionError,))
-    def _retry_confirm_haproxy_state(dbproxy_host, grep_command):
-        dbproxy_host.run_sync(
-            f"echo 'show stat' | socat /run/haproxy/haproxy.sock stdio | {grep_command}"
-        )
-
-    def _repool_host(self, dbproxy_host, clouddb_hostname, section):
-
-        dbproxy_host.run_sync(
-            f'echo "set server {clouddb_hostname} state ready" \
-                | socat /run/haproxy/haproxy.sock stdio'
-        )
-
-        grep_command = f"grep -qz '^mariadb-{section},{clouddb_hostname}'"
-        desired_state = "repooled"
-
-        self._confirm_haproxy_state(
-            dbproxy_host, clouddb_hostname, grep_command, desired_state
-        )
-
-    @staticmethod
-    def _close_connections(clouddb_hostname, section):
-        # TODO this can be automated; perhaps by restarting mysql
-        # applications are required to handle the database disconnecting
-        print(
-            f"Now ssh to host {clouddb_hostname} to close any lingering database connections."
-        )
-        print("Connect to mariadb with the following command:")
-        print(f"    sudo mysql -S /var/run/mysqld/mysqld.{section}.sock")
-        print("Check the processes list in the User column")
-        print("for anything started by users (u####) and services (s####):")
-        print("    MariaDB [(none)]> show processlist;")
-        print('Quit any remaining "userspace" connections using kill <id>')
-
-        ask_confirmation("Have all the user connections been closed?")
 
     @staticmethod
     def _run_sql_test_pre():
@@ -225,16 +147,10 @@ class UpdateWikireplicaViewsRunner(CookbookRunnerBase):
 
         ask_confirmation("Does your sql statement reflect the expected outcome?")
 
-    def _update_views_on_host(self, dbproxy_host, clouddb_hostname, section):
-        self._depool_host(dbproxy_host, clouddb_hostname, section)
-
-        self._close_connections(clouddb_hostname, section)
+    def _update_views_on_host(self, clouddb_hostname):
         self._run_sql_test_pre()
-
         self._run_maintain_views(clouddb_hostname)
-
         self._run_sql_test_post()
-        self._repool_host(dbproxy_host, clouddb_hostname, section)
 
     def rollback(self):
         """Comment on phabricator in case of a failed run."""
@@ -258,11 +174,10 @@ class UpdateWikireplicaViewsRunner(CookbookRunnerBase):
             self.phabricator.task_comment(self.task_id, phab_message)
 
         for category in db_categories:
-            dbproxy_host = self._determine_dbproxy_host_to_update(category)
             clouddb_hostname = self._determine_clouddb_hostname_to_update(
                 self.section, category
             )
-            self._update_views_on_host(dbproxy_host, clouddb_hostname, self.section)
+            self._update_views_on_host(clouddb_hostname)
 
         if self.phabricator is not None:
             self.phabricator.task_comment(
