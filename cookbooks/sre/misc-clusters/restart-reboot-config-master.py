e89d3e1719dfb808af6f29190be9683334a018a2
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-02-06 15:50:09 +0100

    New cookbook to reboot/restart config-master hosts
    Change-Id: Id497e9cf414753ad73b56b72b97d21223a3d60ea

---
+++ cookbooks/sre/misc-clusters/restart-reboot-config-master.py
@@ -0,0 +1,36 @@
+"""Cookbook to perform a rolling reboot of config-master hosts"""
+from cookbooks.sre import SREBatchBase, SREDiscoveryNoLVSBatchRunnerBase
+
+
+class ConfigMasterRestartReboot(SREBatchBase):
+    """Cookbook to perform a rolling reboot of config-master nodes
+
+    Usage example:
+        cookbook sre.discovery.restart-reboot-config-master \
+        --reason "Rolling reboot to pick up new kernel" reboot \
+        --alias config-master
+
+    """
+
+    batch_default = 1
+
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return ConfigMasterRestartRebootRunner(args, self.spicerack)
+
+
+class ConfigMasterRestartRebootRunner(SREDiscoveryNoLVSBatchRunnerBase):
+    """Roll reboot/restart an ConfigMaster replica cluster"""
+
+    # Name of discovery record
+    service_name = 'config-master'
+
+    @property
+    def allowed_aliases(self):
+        """Required by SREBatchRunnerBase"""
+        return ['config-master', 'config-master-eqiad', 'config-master-codfw']
+
+    @property
+    def restart_daemons(self):
+        """Return a list of daemons to restart when using the restart action"""
+        return ['envoyproxy', 'apache2']
