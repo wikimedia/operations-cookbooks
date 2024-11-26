532a34cc4ad16f2ef035dafe59298ad4ca77bee5
Moritz Mühlenhoff <mmuhlenhoff@wikimedia.org>
2024-11-26 10:26:02 +0100

    Temporarily remove ferm node check
    The magru cluster has already been moved to nftables, this will be fixed
    later to support both nftables and ferm, but remove for now to
    unblock the magru server shuffle work.
    
    Change-Id: I4cf9121e1d99877b195b00dcecb543366642f347

--- cookbooks/sre/ganeti/addnode.py
+++ cookbooks/sre/ganeti/addnode.py
@@ -1,6 +1,5 @@
 """Add a new node to a Ganeti cluster"""
 import logging
-import socket
 
 from wmflib.interactive import ask_confirmation, ensure_shell_is_durable
 from spicerack.cookbook import CookbookBase, CookbookRunnerBase
@@ -132,14 +131,6 @@ class GanetiAddNodeRunner(CookbookRunnerBase):
             ('No "ganeti" volume group found. You need to remove the swap device on /dev/md2, '
              'create a PV on /dev/md2 and eventually create a VG named "ganeti". Make sure to '
              'remove the stale swap entry from fstab as well'),
-        )
-
-        host_ip = socket.gethostbyname(self.fqdn)
-        self.validate_state(
-            f'grep {host_ip} /etc/ferm/conf.d/10_ganeti_ssh_cluster',
-            ('The node cannot be found in the Ferm config of the Ganeti master.'
-             'Make sure to add it to the profile::ganeti::nodes Hiera config.'),
-            run_on_masternode=True,
         )
 
         if not self.routed:
