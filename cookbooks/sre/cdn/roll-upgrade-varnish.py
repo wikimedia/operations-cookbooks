bd31d619b75301a9be75b102cfeb0e8e512e3518
Brett Cornwall <bcornwall@wikimedia.org>
2025-03-19 09:29:12 -0700

    cdn: Add roll-upgrade-varnish
    This replaces the upgrade-varnish with a more streamlined
    rolling-upgrade model that follows the same paradigm as the haproxy and
    ats upgrades.
    
    Bug: T389387
    Change-Id: I6b1bc4e3291cd5a1e0ddca88303e9eb2ed23a76a

---
+++ cookbooks/sre/cdn/roll-upgrade-varnish.py
@@ -0,0 +1,93 @@
+"""Upgrade Varnish on CDN nodes."""
+
+from spicerack.administrative import Reason
+from spicerack.remote import RemoteHosts
+from wmflib.constants import ALL_DATACENTERS
+from wmflib.interactive import confirm_on_failure
+from cookbooks.sre import SREBatchBase, SRELBBatchRunnerBase
+
+
+class RollUpgradeVarnish(SREBatchBase):
+    r"""Roll upgrade Varnish in the CDN cluster.
+
+    * Depool
+    * Install Varnish and components, letting apt dictate the versions
+    * Restart related services
+    * Repool
+
+    Example usage:
+
+        cookbook sre.cdn.roll-upgrade-varnish \
+            --alias cp-text_codfw \
+            --reason '7.1 upgrade'
+    """
+
+    # Reboot/Restart wipes the in-memory cache, so a high grace sleep is
+    # required.
+    batch_max = 1
+    grace_sleep = 1500
+    min_grace_sleep = 1200
+    valid_actions = ('upgrade',)
+
+    def get_runner(self, args):
+        """As specified by Spicerack API."""
+        return RollUpgradeVarnishRunner(args, self.spicerack)
+
+
+class RollUpgradeVarnishRunner(SRELBBatchRunnerBase):
+    """Upgrade and restart Varnish"""
+
+    depool_sleep = 60
+
+    def _upgrade_action(self, hosts: RemoteHosts, reason: Reason) -> None:
+        """Install packages"""
+        puppet = self._spicerack.puppet(hosts)
+        puppet.run()
+
+        pkgs = [
+            "libvarnishapi3",
+            "libvmod-netmapper",
+            "libvmod-querysort",
+            "varnish",
+            "varnish-modules",
+            "varnish-re2",
+        ]
+
+        apt_get = self._spicerack.apt_get(hosts)
+        confirm_on_failure(apt_get.update)
+        confirm_on_failure(apt_get.install, ",".join(pkgs))
+
+        self._restart_daemons_action(hosts, reason)
+        # Run any potential corrective measures.
+        puppet.run()
+
+    @property
+    def allowed_aliases(self):
+        """Required by RebootRunnerBase"""
+        aliases = ['cp']
+        for role in ('text', 'upload'):
+            aliases.append(f'cp-{role}')
+            for dc in ALL_DATACENTERS:
+                aliases.append(f'cp-{dc}')
+                aliases.append(f'cp-{role}_{dc}')
+        return aliases
+
+    @property
+    def allowed_aliases_query(self) -> str:
+        """Override the parent property to optimize the query."""
+        return 'A:cp'  # This query must include all hosts matching all the allowed_aliases
+
+    @property
+    def runtime_description(self):
+        """Override the default runtime description"""
+        return f'rolling upgrade of Varnish on {self._query()}'
+
+    @property
+    def restart_daemons(self):
+        """Return a list of daemons to restart when using the restart action"""
+        return [
+            "prometheus-varnish-exporter@frontend",
+            "varnish-frontend",
+            # varnishkafka is linked against libvarnishapi
+            "varnishkafka-all",
+        ]
