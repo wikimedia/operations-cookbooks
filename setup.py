9ccb9e1bcff6df9c14dae61ce16ff5cc1d972147
Federico Ceratto <fceratto@wikimedia.org>
2026-02-18 16:11:04 +0100

    Reflow setup.py using Black
    Change-Id: I23837b4d770489854b8ae9f55f069689ed376973

--- setup.py
+++ setup.py
@@ -5,58 +5,58 @@ from setuptools import find_namespace_packages, setup
 # The below list is only for CI
 # For prod add the libs to modules/profile/manifests/spicerack.pp
 install_requires = [
-    'aiohttp',
-    'cryptography',
-    'packaging',
-    'prettytable',
-    'prometheus-client==0.9.0',
-    'python-dateutil',
-    'python-gitlab==3.11.0',
-    'scapy>=2.4.4',
-    'wikimedia-spicerack',
+    "aiohttp",
+    "cryptography",
+    "packaging",
+    "prettytable",
+    "prometheus-client==0.9.0",
+    "python-dateutil",
+    "python-gitlab==3.11.0",
+    "scapy>=2.4.4",
+    "wikimedia-spicerack",
     # on cumin nodes transferpy v1.1 is installed as the Debian package
-    'transferpy @ git+https://gerrit.wikimedia.org/r/operations/software/transferpy@v1.1',
+    "transferpy @ git+https://gerrit.wikimedia.org/r/operations/software/transferpy@v1.1",
 ]
 
 # Extra dependencies
 extras_require = {
     # Test dependencies
-    'tests': [
-        'bandit>=1.5.0',
-        'flake8>=3.2.1',
-        'mypy>=0.670',
-        'pytest>=6.1.0',
-        'types-PyMySQL',
-        'types-python-dateutil',
-        'types-PyYAML',
-        'types-redis',
+    "tests": [
+        "bandit>=1.5.0",
+        "flake8>=3.2.1",
+        "mypy>=0.670",
+        "pytest>=6.1.0",
+        "types-PyMySQL",
+        "types-python-dateutil",
+        "types-PyYAML",
+        "types-redis",
         # Uppe limit because it requires urllib3>2, incompatible with elasticsearch, elasticsearch-curator and botocore
-        'types-requests<2.31.0.7',
-        'types-setuptools',
+        "types-requests<2.31.0.7",
+        "types-setuptools",
     ],
-    'prospector': [
-        'prospector[with_everything]==1.16.1',  # pinned
-        'pytest>=6.1.0',
+    "prospector": [
+        "prospector[with_everything]==1.16.1",  # pinned
+        "pytest>=6.1.0",
     ],
 }
 
 setup_requires = [
-    'setuptools_scm>=1.15.0',
+    "setuptools_scm>=1.15.0",
 ]
 
 setup(
-    author='Riccardo Coccioli',
-    author_email='rcoccioli@wikimedia.org',
-    description='Wikimedia Foundations production automation and orchestration cookbooks',
+    author="Riccardo Coccioli",
+    author_email="rcoccioli@wikimedia.org",
+    description="Wikimedia Foundations production automation and orchestration cookbooks",
     extras_require=extras_require,
     install_requires=install_requires,
-    keywords=['wmf', 'automation', 'orchestration', 'cookbooks'],
-    license='GPLv3+',
-    name='wikimedia-cookbooks',
-    packages=find_namespace_packages(include=['cookbooks'], exclude=['*.tests', '*.tests.*']),
-    platforms=['GNU/Linux'],
+    keywords=["wmf", "automation", "orchestration", "cookbooks"],
+    license="GPLv3+",
+    name="wikimedia-cookbooks",
+    packages=find_namespace_packages(include=["cookbooks"], exclude=["*.tests", "*.tests.*"]),
+    platforms=["GNU/Linux"],
     setup_requires=setup_requires,
     use_scm_version=True,
-    url='https://github.com/wikimedia/operations-cookbooks',
+    url="https://github.com/wikimedia/operations-cookbooks",
     zip_safe=False,
 )
